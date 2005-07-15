d6e1252ce2b7d0f147baa5748b6663982ff1c3fd
pharaohh <pharaoh@apache.org>
2005-07-15 16:16:51 +0000

    Updated class files w/ LGPL header
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710230 13f79535-47bb-0310-9956-ffa450edef68

--- jsecurity/api/src/org/jsecurity/session/SessionManager.java
+++ jsecurity/api/src/org/jsecurity/session/SessionManager.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (C) 2005 Les A. Hazlewood
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the
+ *
+ * Free Software Foundation, Inc.
+ * 59 Temple Place, Suite 330
+ * Boston, MA 02111-1307
+ * USA
+ *
+ * Or, you may view it online at
+ * http://www.opensource.org/licenses/lgpl-license.php
+ */
 package org.jsecurity.session;
 
 import java.io.Serializable;
@@ -12,6 +36,8 @@ import java.security.Principal;
  * <p>All interaction with a secure system is done in the course of a Session, even if that
  * Session only exists over the course of a single method invocation.  Sessions are extremely
  * lightweight objects that have a managed lifecycle.
+ *
+ * @author Les Hazlewood
  */
 public interface SessionManager {
 
