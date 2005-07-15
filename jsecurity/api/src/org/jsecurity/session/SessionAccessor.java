f339cb98d967b1f34514a6a899f60d3adc2a25ae
pharaohh <dev-null@apache.org>
2005-07-15 16:16:51 +0000

    Updated class files w/ LGPL header
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710230 13f79535-47bb-0310-9956-ffa450edef68

--- jsecurity/api/src/org/jsecurity/session/SessionAccessor.java
+++ jsecurity/api/src/org/jsecurity/session/SessionAccessor.java
@@ -1,11 +1,34 @@
+/*
+ * Copyright (C) 2005 Les A. Hazlewood
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the
+ *
+ * Free Software Foundation, Inc.
+ * 59 Temple Place, Suite 330
+ * Boston, MA 02111-1307
+ * USA
+ *
+ * Or, you may view it online at
+ * http://www.opensource.org/licenses/lgpl-license.php
+ */
 package org.jsecurity.session;
 
 import java.net.InetAddress;
 import java.io.Serializable;
 
 /**
- * Created by IntelliJ IDEA. User: lhazlewood Date: Jul 15, 2005 Time: 10:33:35 AM To change this
- * template use File | Settings | File Templates.
+ * @author Les Hazlewood
  */
 public interface SessionAccessor {
 
