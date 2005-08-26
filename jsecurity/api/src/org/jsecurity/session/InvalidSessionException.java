c9510b85dc4b86fb2ba9ed955fa6ea6134760f6f
pharaohh <pharaoh@apache.org>
2005-08-26 15:20:38 +0000

    updated build files and added jarjar task
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710255 13f79535-47bb-0310-9956-ffa450edef68

--- jsecurity/api/src/org/jsecurity/session/InvalidSessionException.java
+++ jsecurity/api/src/org/jsecurity/session/InvalidSessionException.java
@@ -24,29 +24,33 @@
  */
 package org.jsecurity.session;
 
-import org.jsecurity.JSecurityException;
-
 /**
- * General security exception attributed to problems during interaction with the system during
- * a session.
+ * Exception thrown when attempting to interact with the system under an established session
+ * when that session is considered invalid.  The meaning of the term 'invalid' is based on
+ * application behavior.  For example, a Session is considered invalid if it has been explicitly
+ * stopped (e.g. in the event of a user log-out or when explicitly
+ * {@link org.jsecurity.session.Session#stop() stopped} programmatically.  A Session can also be
+ * considered invalid if it has expired.
+ * 
+ * @see ExpiredSessionException
  *
  * @author Les Hazlewood
  */
-public class SessionException extends JSecurityException {
+public class InvalidSessionException extends SessionException {
 
-    public SessionException() {
+    public InvalidSessionException() {
         super();
     }
 
-    public SessionException( String s ) {
+    public InvalidSessionException( String s ) {
         super( s );
     }
 
-    public SessionException( String message, Throwable cause ) {
+    public InvalidSessionException( String message, Throwable cause ) {
         super( message, cause );
     }
 
-    public SessionException( Throwable cause ) {
+    public InvalidSessionException( Throwable cause ) {
         super( cause );
     }
 
