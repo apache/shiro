43e0faf9c6fa5371cfa30844448cc9d193dfad4e
pharaohh <pharaoh@apache.org>
2005-10-05 20:58:40 +0000

    added WebSessionFactory implementation
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710269 13f79535-47bb-0310-9956-ffa450edef68

--- jsecurity/ri/business/src/org/jsecurity/web/support/DefaultWebSessionFactory.java
+++ jsecurity/ri/business/src/org/jsecurity/web/support/DefaultWebSessionFactory.java
@@ -3,8 +3,12 @@ package org.jsecurity.web.support;
 import org.jsecurity.web.WebSessionFactory;
 import org.jsecurity.session.Session;
 import org.jsecurity.session.SessionFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * Default JSecurity Reference Implementation of the {@link WebSessionFactory} interface.
@@ -12,6 +16,8 @@ import javax.servlet.http.HttpServletRequest;
  * @author Les Hazlewood
  */
 public class DefaultWebSessionFactory implements WebSessionFactory {
+
+    protected transient final Log log = LogFactory.getLog( getClass() );
 
     SessionFactory sessionFactory;
 
@@ -28,11 +34,29 @@ public class DefaultWebSessionFactory implements WebSessionFactory {
         }
     }
 
+    protected InetAddress getInetAddress( HttpServletRequest request ) {
+        InetAddress clientAddress = null;
+        //get the Host/IP they're coming from:
+        String addrString = request.getRemoteHost();
+        try {
+            clientAddress = InetAddress.getByName( addrString );
+        } catch ( UnknownHostException e ) {
+            if ( log.isWarnEnabled() ) {
+                log.warn( "Unable to acquire InetAddress from HttpServletRequest, " +
+                          "using null for Session creation", e );
+            }
+        }
+
+        return clientAddress;
+    }
+
     public Session start( HttpServletRequest request ) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        InetAddress clientAddress = getInetAddress( request );
+        return sessionFactory.start( clientAddress );
     }
 
     public Session getSession( HttpServletRequest request ) {
+        //todo - implement
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
