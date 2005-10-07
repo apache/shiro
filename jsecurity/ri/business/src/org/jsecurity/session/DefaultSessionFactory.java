5986c7cb9b5473900c84861d1903fd9ebadb39db
pharaohh <dev-null@apache.org>
2005-10-07 21:15:06 +0000

    updated build to include spring support.  Added default SessionFactory implementation
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710279 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jsecurity/ri/business/src/org/jsecurity/session/DefaultSessionFactory.java
@@ -0,0 +1,69 @@
+package org.jsecurity.session;
+
+import org.jsecurity.authz.HostUnauthorizedException;
+import org.jsecurity.authz.AuthorizationException;
+
+import java.net.InetAddress;
+import java.io.Serializable;
+
+/**
+ * Default JSecurity reference implementation of a {@link SessionFactory}.  This implementation
+ * returns instances where all methods delegate to a corresponding
+ * {@link SessionManager SessionManager} method call.  That is, the objects returned act as
+ * transparent proxies to the SessionManager responsible for all Sessions in a system.
+ *
+ * <p>Objects returned from this factory implementation should not be cached on the
+ * business/server tier (e.g. in an HttpSession or in some local {@link java.util.Map Map} instance).
+ * All instances should be acquired from this {@link SessionFactory SessionFactory} at the
+ * beginning of a thread's exucution, such as at the beginning of a remote method invocation or at
+ * the beginning of an Http request.  They are extremely lightweight and are designed to be created
+ * as needed.
+ *
+ * <p>This transparent proxy/delegate technique allows the JSecurity reference implementation to
+ * maintain a stateless architecture (i.e. similar to accessing a Stateless Session Bean EJB),
+ * which is extremely efficient.  Any state to be maintained is the responsibility of the
+ * SessionManager, not your application, thereby aking your code much cleaner and more efficient.
+ *
+ * @author Les Hazlewood
+ */
+public class DefaultSessionFactory implements SessionFactory {
+
+    private SessionManager sessionManager = null;
+
+    public DefaultSessionFactory(){}
+
+    public void setSessionManager( SessionManager sessionManager ) {
+        this.sessionManager = sessionManager;
+    }
+
+    public void init() {
+        if ( this.sessionManager == null ) {
+            String msg = "sessionManager property must be set";
+            throw new IllegalStateException( msg );
+        }
+    }
+
+    public Session start( InetAddress hostAddress )
+        throws HostUnauthorizedException, IllegalArgumentException {
+
+        Serializable sessionId = sessionManager.start( hostAddress );
+        return new SessionHandle( sessionManager, sessionId );
+    }
+
+    public Session getSession( Serializable sessionId )
+        throws InvalidSessionException, AuthorizationException {
+
+        if ( sessionManager.isExpired( sessionId ) ) {
+            String msg = "Session with id [" + sessionId + "] has expired and may not " +
+                         "be used.";
+            throw new ExpiredSessionException( msg );
+        } else if ( sessionManager.isStopped( sessionId ) ) {
+            String msg = "Session with id [" + sessionId + "] has been stopped and may not " +
+                         "be used.";
+            throw new StoppedSessionException( msg );
+        }
+
+        return new SessionHandle( sessionManager, sessionId );
+    }
+
+}
