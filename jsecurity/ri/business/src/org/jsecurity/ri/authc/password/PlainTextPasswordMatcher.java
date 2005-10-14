a64a94e9f672ffecbe48ca55f3004ccdcb929de5
mentiro <dev-null@apache.org>
2005-10-14 03:16:07 +0000

    Cleaned up comments.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710300 13f79535-47bb-0310-9956-ffa450edef68

--- jsecurity/ri/business/src/org/jsecurity/ri/authc/password/PlainTextPasswordMatcher.java
+++ jsecurity/ri/business/src/org/jsecurity/ri/authc/password/PlainTextPasswordMatcher.java
@@ -33,26 +33,6 @@ package org.jsecurity.ri.authc.password;
  */
 public class PlainTextPasswordMatcher implements PasswordMatcher {
 
-    /*--------------------------------------------
-    |             C O N S T A N T S             |
-    ============================================*/
-
-    /*--------------------------------------------
-    |    I N S T A N C E   V A R I A B L E S    |
-    ============================================*/
-
-    /*--------------------------------------------
-    |         C O N S T R U C T O R S           |
-    ============================================*/
-
-    /*--------------------------------------------
-    |  A C C E S S O R S / M O D I F I E R S    |
-    ============================================*/
-
-    /*--------------------------------------------
-    |               M E T H O D S               |
-    ============================================*/
-
 
     public boolean doPasswordsMatch(char[] providedPassword, char[] storedPassword) {
         if( providedPassword.length != storedPassword.length ) {
