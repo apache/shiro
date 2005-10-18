675eba88ba03f6a58f50811fb83c4125d3e920d8
pharaohh <pharaoh@apache.org>
2005-10-18 20:53:36 +0000

    updated implementation to utilize the Spring RedirectView class to handle redirect logic
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710305 13f79535-47bb-0310-9956-ffa450edef68

--- jsecurity/support/spring/src/org/jsecurity/spring/servlet/security/AuthenticationInterceptor.java
+++ jsecurity/support/spring/src/org/jsecurity/spring/servlet/security/AuthenticationInterceptor.java
@@ -1,6 +1,7 @@
 package org.jsecurity.spring.servlet.security;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.beans.factory.InitializingBean;
 import org.jsecurity.authz.AuthorizationContext;
 import org.jsecurity.context.SecurityContext;
@@ -16,14 +17,24 @@ import javax.servlet.http.HttpSession;
  * Simple interceptor that verifies a user is authenticated (logged-in) before allowing a
  * page to be viewd.
  *
- * <p>If the user is not authenticated, they will be forwarded to the login page located
- * at the URL {@link #getLoginURL() getLoginURL()}.  Just prior to being forwarded, the
+ * <p>If the user is not authenticated, they will be redirected to the login page located
+ * at the URL {@link #getLoginURL() getLoginURL()}.  Just prior to being redirected, the
  * page URL they attempted to view is first saved in the <tt>HttpSession</tt> under the
  * key {@link WebUtils#ATTEMPTED_PAGE_KEY}.
  *
  * <p>Upon a successful login, the login controller may
  * use this session key to foward the user to the page they were attempting to view prior to
  * logging in, a nice usability feature.
+ *
+ * <p>The default redirect implementation is accomplished via a
+ * Spring {@link RedirectView RedirectView}, {@link RedirectView#render rendered} with
+ * a <tt>null</tt> model <tt>Map</tt>.  The
+ * {@link #setContextRelative contextRelative}, {@link #setHttp10Compatible http10Compatible} and
+ * {@link #setEncodingScheme encodingScheme} are passthrough attributes that will be set on the
+ * <tt>RedirectView</tt> that is created to process the redirect.  Set them according to the
+ * {@link RedirectView RedirectView} JavaDoc.
+ *
+ * @see RedirectView RedirectView
  *
  * @author Les Hazlewood
  */
@@ -34,6 +45,12 @@ public class AuthenticationInterceptor extends HandlerInterceptorAdapter
 
     private String loginURL = null;
 
+    private boolean contextRelative = false;
+
+	private boolean http10Compatible = true;
+
+	private String encodingScheme = RedirectView.DEFAULT_ENCODING_SCHEME;
+
     public AuthenticationInterceptor(){}
 
     public String getLoginURL() {
@@ -43,6 +60,42 @@ public class AuthenticationInterceptor extends HandlerInterceptorAdapter
     public void setLoginURL( String loginURL ) {
         this.loginURL = loginURL;
     }
+
+    /**
+	 * Set whether to interpret a given URL that starts with a slash ("/")
+	 * as relative to the current ServletContext, i.e. as relative to the
+	 * web application root.
+	 * <p>Default is "false": A URL that starts with a slash will be interpreted
+	 * as absolute, i.e. taken as-is. If true, the context path will be
+	 * prepended to the URL in such a case.
+	 * @see javax.servlet.http.HttpServletRequest#getContextPath
+	 */
+	public void setContextRelative(boolean contextRelative) {
+		this.contextRelative = contextRelative;
+	}
+
+	/**
+	 * Set whether a redirect will stay compatible with HTTP 1.0 clients.
+	 * <p>In the default implementation, this will enforce HTTP status code 302
+	 * in any case, i.e. delegate to <code>HttpServletResponse.sendRedirect</code>.
+	 * Turning this off will send HTTP status code 303, which is the correct
+	 * code for HTTP 1.1 clients, but not understood by HTTP 1.0 clients.
+	 * <p>Many HTTP 1.1 clients treat 302 just like 303, not making any
+	 * difference. However, some clients depend on 303 when redirecting
+	 * after a POST request; turn this flag off in such a scenario.
+	 * @see javax.servlet.http.HttpServletResponse#sendRedirect
+	 */
+	public void setHttp10Compatible(boolean http10Compatible) {
+		this.http10Compatible = http10Compatible;
+	}
+
+	/**
+	 * Set the encoding scheme for the redirect.
+	 */
+	public void setEncodingScheme(String encodingScheme) {
+		this.encodingScheme = encodingScheme;
+	}
+
 
     public void afterPropertiesSet() throws Exception {
         if ( getLoginURL() == null ) {
@@ -69,13 +122,15 @@ public class AuthenticationInterceptor extends HandlerInterceptorAdapter
 
             if ( log.isDebugEnabled() ) {
                 log.debug( "User is not allowed to access page [" + attemptedPage + "] without " +
-                           "first being authenticated.  Forwarding to login page [" +
+                           "first being authenticated.  Redirecting to login page [" +
                            getLoginURL() + "]");
             }
 
-            response.sendRedirect( getLoginURL() );
+            RedirectView redirect =
+                new RedirectView( getLoginURL(), this.contextRelative, this.http10Compatible );
+            redirect.setEncodingScheme( this.encodingScheme );
+            redirect.render( null, request, response );
             return false;
-
         } else {
             return true;
         }
