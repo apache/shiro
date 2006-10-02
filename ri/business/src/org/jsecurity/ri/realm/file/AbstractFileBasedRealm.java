7e5b37b654ce3e5c913d1e0386beed0d76f8a4b4
mentiro <dev-null@apache.org>
2006-10-02 03:21:54 +0000

    Added realm classes, but haven't implemented anything.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710421 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ri/business/src/org/jsecurity/ri/realm/file/AbstractFileBasedRealm.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2006 Jeremy Haile
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the
+ *
+ * Free Software Foundation, Inc.
+ * 59 Temple Place, Suite 330
+ * Boston, MA 02111-1307
+ * USA
+ *
+ * Or, you may view it online at
+ * http://www.opensource.org/licenses/lgpl-license.php
+ */
+
+package org.jsecurity.ri.realm.file;
+
+/**
+ * Description of class.
+ *
+ * @since 0.2
+ * @author Jeremy Haile
+ */
+public class AbstractFileBasedRealm {
+
+    /*--------------------------------------------
+    |             C O N S T A N T S             |
+    ============================================*/
+
+    /*--------------------------------------------
+    |    I N S T A N C E   V A R I A B L E S    |
+    ============================================*/
+
+    /*--------------------------------------------
+    |         C O N S T R U C T O R S           |
+    ============================================*/
+
+    /*--------------------------------------------
+    |  A C C E S S O R S / M O D I F I E R S    |
+    ============================================*/
+
+    /*--------------------------------------------
+    |               M E T H O D S               |
+    ============================================*/
+}
