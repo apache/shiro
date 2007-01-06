e2640a5e9e622f4169da86e36b7faf645ab02887
pharaohh <pharaoh@apache.org>
2007-01-06 01:21:31 +0000

    minor build changes to build spring and jboss support
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710453 13f79535-47bb-0310-9956-ffa450edef68

---
+++ support/jboss/src/org/jsecurity/ri/jboss/aop/JBossAopAuthorizationInterceptor.java
@@ -0,0 +1,61 @@
+package org.jsecurity.ri.jboss.aop;
+
+import org.jboss.aop.advice.Interceptor;
+import org.jboss.aop.joinpoint.Invocation;
+import org.jboss.aop.joinpoint.MethodInvocation;
+import org.jsecurity.authz.AuthorizedAction;
+import org.jsecurity.authz.Authorizer;
+import org.jsecurity.authz.aop.AbstractAuthorizationInterceptor;
+
+import java.lang.reflect.Method;
+
+/**
+ * @since 0.2
+ * @author Les Hazlewood
+ */
+public class JBossAopAuthorizationInterceptor
+        extends AbstractAuthorizationInterceptor implements Interceptor {
+
+    private static final String NAME = "JSecurity JBossAopAuthorizationInterceptor";
+
+    public JBossAopAuthorizationInterceptor(){}
+
+    public JBossAopAuthorizationInterceptor( Authorizer authorizer ) {
+        setAuthorizer( authorizer );
+    }
+
+    public String getName() {
+        return NAME;
+    }
+
+    protected AuthorizedAction createAuthzAction( Object jbossAopInvocation ) {
+        final MethodInvocation mi = (MethodInvocation)jbossAopInvocation;
+
+        org.jsecurity.authz.method.MethodInvocation jsecurityMI =
+                new org.jsecurity.authz.method.MethodInvocation() {
+                    public Method getMethod() {
+                        return mi.getMethod();
+                    }
+
+                    public Object[] getArguments() {
+                        return mi.getArguments();
+                    }
+
+                    public String toString() {
+                        return "Method invocation [" + mi.getMethod() + "]";
+                    }
+                };
+
+        return jsecurityMI;
+    }
+
+    protected Object continueInvocation( Object jbossAopInvocation ) throws Throwable {
+        Invocation invocation = (Invocation)jbossAopInvocation;
+        return invocation.invokeNext();
+    }
+
+    public Object invoke( final Invocation invocation ) throws Throwable {
+        return super.invoke( invocation );
+    }
+
+}
