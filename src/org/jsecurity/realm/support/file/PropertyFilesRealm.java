4157331161a38fdf0dad910e5d5c2eb3e65d4e7e
mentiro <mentiro@apache.org>
2007-02-01 22:34:20 +0000

    Initial commit of property file support - still needs more work.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710481 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/jsecurity/realm/support/file/PropertyFilesRealm.java
+++ src/org/jsecurity/realm/support/file/PropertyFilesRealm.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006 Jeremy Haile
+ * Copyright (C) 2005-2007 Jeremy Haile
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -25,13 +25,27 @@
 
 package org.jsecurity.realm.support.file;
 
+import org.jsecurity.authc.AuthenticationException;
+import org.jsecurity.authc.AuthenticationInfo;
+import org.jsecurity.authc.AuthenticationToken;
+import org.jsecurity.realm.support.AbstractCachingRealm;
+import org.jsecurity.realm.support.AuthorizationInfo;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.Principal;
+import java.util.Properties;
+
 /**
- * Description of class.
+ * A simple file-based <tt>Realm</tt> that can be used to implement role-based security with
+ * a set of users defined in a properties file.
  *
+ * TODO THIS IS A WORK IN PROGRESS - LES, IM NOT DONE.
  * @since 0.2
  * @author Jeremy Haile
  */
-public class AbstractFileBasedRealm {
+public class PropertyFilesRealm extends AbstractCachingRealm {
 
     /*--------------------------------------------
     |             C O N S T A N T S             |
@@ -40,6 +54,11 @@ public class AbstractFileBasedRealm {
     /*--------------------------------------------
     |    I N S T A N C E   V A R I A B L E S    |
     ============================================*/
+    private boolean useXmlFormat = false;
+
+    private String userFile;
+
+    private String permissionsFile;
 
     /*--------------------------------------------
     |         C O N S T R U C T O R S           |
@@ -52,4 +71,36 @@ public class AbstractFileBasedRealm {
     /*--------------------------------------------
     |               M E T H O D S               |
     ============================================*/
+    public void onInit() {
+        loadProperties();
+    }
+
+    private void loadProperties() {
+        Properties props = new Properties();
+        try {
+
+            InputStream userStream = new FileInputStream( userFile );
+
+            if( useXmlFormat ) {
+                props.loadFromXML( userStream );
+            } else {
+                props.load( userStream );
+            }
+        } catch( IOException e ) {
+            //todo throw exception here
+        }
+    }
+
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+
+
+
+
+        return null;
+    }
+
+    protected AuthorizationInfo doGetAuthorizationInfo(Principal principal) {
+        return null;
+    }
+
 }
