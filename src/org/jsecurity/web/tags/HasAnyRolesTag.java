98c67c4993e3480ae0341b0027b384489ddaface
mentiro <mentiro@apache.org>
2007-06-20 02:36:38 +0000

    Added HasAnyRoles tag.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710539 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/jsecurity/web/tags/HasAnyRolesTag.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2005-2007 Jeremy Haile
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the
+ *
+ * Free Software Foundation, Inc.
+ * 59 Temple Place, Suite 330
+ * Boston, MA 02111-1307
+ * USA
+ *
+ * Or, you may view it online at
+ * http://www.opensource.org/licenses/lgpl-license.php
+ */
+package org.jsecurity.web.tags;
+
+import org.jsecurity.context.SecurityContext;
+
+/**
+ * Displays body content if the current user has any of the roles specified.
+ * @since 0.2
+ * @author Jeremy Haile
+ */
+public class HasAnyRolesTag extends RoleTag {
+
+    // Delimeter that separates role names in tag attribute
+    private static final String ROLE_NAMES_DELIMETER = ",";
+
+    public HasAnyRolesTag(){}
+
+    protected boolean showTagBody( String roleNames ) {
+        boolean hasAnyRole = false;
+
+        SecurityContext securityContext = getSecurityContext();
+
+        if( securityContext != null ) {
+
+            // Iterate through roles and check to see if the user has one of the roles
+            for( String role : roleNames.split(ROLE_NAMES_DELIMETER) ) {
+
+                if( securityContext.hasRole( role ) ) {
+                    hasAnyRole = true;
+                    break;
+                }
+
+            }
+
+        }
+
+        return hasAnyRole;
+    }
+
+}
