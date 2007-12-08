d668db39db373622177d0fde08dc41b29ad23df2
pharaohh <pharaoh@apache.org>
2007-12-08 01:09:35 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710639 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/jsecurity/web/support/WebSecurityContext.java
+++ src/org/jsecurity/web/support/WebSecurityContext.java
@@ -17,48 +17,36 @@ import java.util.List;
  */
 public class WebSecurityContext extends DelegatingSecurityContext {
 
-    private boolean webSessions = true;
     private ServletRequest servletRequest = null;
 
-    public WebSecurityContext( boolean authenticated, InetAddress inetAddress, Session session, SecurityManager securityManager, ServletRequest request ) {
-        super( authenticated, inetAddress, session, securityManager );
+    public WebSecurityContext(boolean authenticated, InetAddress inetAddress, Session session, SecurityManager securityManager, ServletRequest request) {
+        super(authenticated, inetAddress, session, securityManager);
         this.servletRequest = request;
     }
 
-    public WebSecurityContext( Object principal, boolean authenticated, InetAddress inetAddress, Session session, SecurityManager securityManager, ServletRequest request ) {
-        super( principal, authenticated, inetAddress, session, securityManager );
+    public WebSecurityContext(Object principal, boolean authenticated, InetAddress inetAddress, Session session, SecurityManager securityManager, ServletRequest request) {
+        super(principal, authenticated, inetAddress, session, securityManager);
         this.servletRequest = request;
     }
 
-    public WebSecurityContext( List<?> principals, boolean authenticated, InetAddress inetAddress, Session session, SecurityManager securityManager, ServletRequest request ) {
-        super( principals, authenticated, inetAddress, session, securityManager );
+    public WebSecurityContext(List<?> principals, boolean authenticated, InetAddress inetAddress, Session session, SecurityManager securityManager, ServletRequest request) {
+        super(principals, authenticated, inetAddress, session, securityManager);
         this.servletRequest = request;
     }
 
-    public boolean isWebSessions() {
-        return this.webSessions;
-    }
-
-    public void setWebSessions(boolean webSessions) {
-        this.webSessions = webSessions;
-    }
-
-    public Session getSession( boolean create ) {
-        if ( isWebSessions() ) {
-            assertValid();
-            if ( this.session == null ) {
-                if ( !( this.servletRequest instanceof HttpServletRequest ) ) {
-                    String msg = "The " + getClass().getName() + " implementation currently only works with HttpServletRequests.";
-                    throw new IllegalStateException( msg );
-                }
-                HttpSession httpSession = ( (HttpServletRequest)this.servletRequest ).getSession( create );
-                if ( httpSession != null ) {
-                    this.session = new WebSession( httpSession, SecurityWebSupport.getInetAddress( this.servletRequest ) );
-                }
+    public Session getSession(boolean create) {
+        assertValid();
+        if (this.session == null) {
+            if (!(this.servletRequest instanceof HttpServletRequest)) {
+                String msg = "The " + getClass().getName() + " implementation currently only works with HttpServletRequests.";
+                throw new IllegalStateException(msg);
             }
-            return this.session;
-        } else {
-            return super.getSession( create );
+            HttpSession httpSession = ((HttpServletRequest) this.servletRequest).getSession(create);
+            if (httpSession != null) {
+                this.session = new WebSession(httpSession, SecurityWebSupport.getInetAddress(this.servletRequest));
+            }
         }
+        return this.session;
+
     }
 }
