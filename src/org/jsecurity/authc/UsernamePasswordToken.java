dc55e1c43be591fe9107245800dd903720e59477
pharaohh <pharaoh@apache.org>
2008-01-17 17:36:06 +0000

    initial RememberMeAuthenticationToken support
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710661 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/jsecurity/authc/UsernamePasswordToken.java
+++ src/org/jsecurity/authc/UsernamePasswordToken.java
@@ -28,8 +28,9 @@ import java.net.InetAddress;
 
 /**
  * <p>A simple username/password authentication token to support the most widely-used authentication mechanism.  This
- * class also implements the <tt>InetAuthenticationToken</tt> interface to retain the IP address location from where
- * the authentication attempt is occuring, in case this information is desireable for an application.</p>
+ * class also implements the <tt>RememberMeAuthenticationToken</tt> and <tt>InetAuthenticationToken</tt> interfaces to
+ * support &quot;Remember Me&quot; services across user sessions and to retain the IP address location from where the
+ * authentication attempt is occuring.<p>
  *
  * <p>Note that this class stores a password as a char[] instead of a String
  * (which may seem more logical).  This is because Strings are immutable and their
@@ -44,7 +45,7 @@ import java.net.InetAddress;
  * @author Jeremy Haile
  * @author Les Hazlewood
  */
-public class UsernamePasswordToken implements InetAuthenticationToken {
+public class UsernamePasswordToken implements InetAuthenticationToken, RememberMeAuthenticationToken {
 
     /*--------------------------------------------
     |             C O N S T A N T S             |
@@ -56,6 +57,8 @@ public class UsernamePasswordToken implements InetAuthenticationToken {
     private String username;
 
     private char[] password;
+
+    private boolean rememberMe = false;
 
     private InetAddress inetAddress;
 
@@ -70,17 +73,20 @@ public class UsernamePasswordToken implements InetAuthenticationToken {
 
     /**
      * Constructs a new UsernamePasswordToken encapsulating the username and password submitted
-     * during an authentication attempt, with a <tt>null</tt> {@link #getInetAddress() inetAddress}.
+     * during an authentication attempt, with a <tt>null</tt> {@link #getInetAddress() inetAddress} and a
+     * <tt>rememberMe</tt> default of <tt>false</tt>.
+     *
      * @param username the username submitted for authentication
      * @param password the password character array submitted for authentication
      */
     public UsernamePasswordToken(final String username, final char[] password) {
-        this( username, password, null );
+        this( username, password, false, null );
     }
 
     /**
      * Constructs a new UsernamePasswordToken encapsulating the username and password submitted
-     * during an authentication attempt, with a <tt>null</tt> {@link #getInetAddress() inetAddress}
+     * during an authentication attempt, with a <tt>null</tt> {@link #getInetAddress() inetAddress} and 
+     * a <tt>rememberMe</tt> default of <tt>false</tt>
      *
      * <p>This is a convience constructor and maintains the password internally via a character
      * array, i.e. <tt>password.toCharArray();</tt>.  Note that storing a password as a String
@@ -90,12 +96,12 @@ public class UsernamePasswordToken implements InetAuthenticationToken {
      * @param password the password string submitted for authentication
      */
     public UsernamePasswordToken(final String username, final String password ) {
-        this( username, password.toCharArray(), null );
+        this( username, password.toCharArray(), false, null );
     }
 
     /**
-     * Constructs a new UsernamePasswordToken encapsulating the username and password submitted, as well as the
-     * inetAddress from where the attempt is occurring.
+     * Constructs a new UsernamePasswordToken encapsulating the username and password submitted, the
+     * inetAddress from where the attempt is occurring, and a default <tt>rememberMe</tt> value of <tt>false</tt>
      *
      * @param username the username submitted for authentication
      * @param password the password string submitted for authentication
@@ -104,14 +110,12 @@ public class UsernamePasswordToken implements InetAuthenticationToken {
      * @since 0.2
      */
     public UsernamePasswordToken(final String username, final char[] password, final InetAddress inetAddress ) {
-        this.username = username;
-        this.password = password;
-        this.inetAddress = inetAddress;
+        this( username, password, false, inetAddress );
     }
 
     /**
-     * Constructs a new UsernamePasswordToken encapsulating the username and password submitted, as well as the
-     * inetAddress from where the attempt is occurring.
+     * Constructs a new UsernamePasswordToken encapsulating the username and password submitted, the
+     * inetAddress from where the attempt is occurring, and a default <tt>rememberMe</tt> value of <tt>false</tt>
      *
      * <p>This is a convience constructor and maintains the password internally via a character
      * array, i.e. <tt>password.toCharArray();</tt>.  Note that storing a password as a String
@@ -124,7 +128,79 @@ public class UsernamePasswordToken implements InetAuthenticationToken {
      * @since 0.2
      */
     public UsernamePasswordToken( final String username, final String password, final InetAddress inetAddress ) {
-        this( username, password.toCharArray(), inetAddress );
+        this( username, password.toCharArray(), false, inetAddress );
+    }
+
+    /**
+     * Constructs a new UsernamePasswordToken encapsulating the username and password submitted, as well as if the user
+     * wishes their identity to be remembered across sessions.
+     *
+     * @param username the username submitted for authentication
+     * @param password the password string submitted for authentication
+     * @param rememberMe if the user wishes their identity to be remembered across sessions
+     * 
+     * @since 1.0
+     */
+    public UsernamePasswordToken(final String username, final char[] password, final boolean rememberMe ) {
+        this( username, password, rememberMe, null );
+    }
+
+    /**
+     * Constructs a new UsernamePasswordToken encapsulating the username and password submitted, as well as if the user
+     * wishes their identity to be remembered across sessions.
+     *
+     * <p>This is a convience constructor and maintains the password internally via a character
+     * array, i.e. <tt>password.toCharArray();</tt>.  Note that storing a password as a String
+     * in your code could have possible security implications as noted in the class JavaDoc.</p>
+     *
+     * @param username the username submitted for authentication
+     * @param password the password string submitted for authentication
+     * @param rememberMe if the user wishes their identity to be remembered across sessions
+     *
+     * @since 1.0
+     */
+    public UsernamePasswordToken( final String username, final String password, final boolean rememberMe ) {
+        this( username, password.toCharArray(), rememberMe, null );
+    }
+
+    /**
+     * Constructs a new UsernamePasswordToken encapsulating the username and password submitted, if the user
+     * wishes their identity to be remembered across sessions, and the inetAddress from where the attempt is ocurring.
+     *
+     * @param username the username submitted for authentication
+     * @param password the password character array submitted for authentication
+     * @param rememberMe if the user wishes their identity to be remembered across sessions
+     * @param inetAddress the inetAddress from where the attempt is occuring
+     *
+     * @since 1.0
+     */
+    public UsernamePasswordToken( final String username, final char[] password,
+                                  final boolean rememberMe, final InetAddress inetAddress ) {
+        this.username = username;
+        this.password = password;
+        this.rememberMe = rememberMe;
+        this.inetAddress = inetAddress;
+    }
+
+
+    /**
+     * Constructs a new UsernamePasswordToken encapsulating the username and password submitted, if the user
+     * wishes their identity to be remembered across sessions, and the inetAddress from where the attempt is ocurring.
+     *
+     * <p>This is a convience constructor and maintains the password internally via a character
+     * array, i.e. <tt>password.toCharArray();</tt>.  Note that storing a password as a String
+     * in your code could have possible security implications as noted in the class JavaDoc.</p>
+     *
+     * @param username the username submitted for authentication
+     * @param password the password string submitted for authentication
+     * @param rememberMe if the user wishes their identity to be remembered across sessions
+     * @param inetAddress the inetAddress from where the attempt is occuring
+     *
+     * @since 1.0
+     */
+    public UsernamePasswordToken( final String username, final String password,
+                                  final boolean rememberMe, final InetAddress inetAddress ) {
+        this( username, password.toCharArray(), rememberMe, inetAddress );
     }
 
 
@@ -211,17 +287,42 @@ public class UsernamePasswordToken implements InetAuthenticationToken {
         this.inetAddress = inetAddress;
     }
 
+    /**
+     * Returns <tt>true</tt> if the submitting user wishes their identity (principal(s)) to be remembered
+     * across sessions, <tt>false</tt> otherwise.  Unless overridden, this value is <tt>false</tt> by default.
+     * @return <tt>true</tt> if the submitting user wishes their identity (principal(s)) to be remembered
+     * across sessions, <tt>false</tt> otherwise (<tt>false</tt> by default).
+     *
+     * @since 1.0
+     */
+    public boolean isRememberMe() {
+        return rememberMe;
+    }
+
+    /**
+     * Sets if the submitting user wishes their identity (pricipal(s)) to be remembered across sessions.  Unless
+     * overridden, the default value is <tt>false</tt>, indicating <em>not</em> to be remembered across sessions.
+     * @param rememberMe value inidicating if the user wishes their identity (principal(s)) to be remembered across
+     * sessions.
+     *
+     * @since 1.0
+     */
+    public void setRememberMe(boolean rememberMe) {
+        this.rememberMe = rememberMe;
+    }
+
     /*--------------------------------------------
     |               M E T H O D S               |
     ============================================*/
 
     /**
-     * Clears out (nulls) the username and password and inetAddress.  The password bytes are explicitly set to
+     * Clears out (nulls) the username, password, rememberMe, and inetAddress.  The password bytes are explicitly set to
      * <tt>0x00</tt> before nulling to eliminate the possibility of memory access at a later time.
      */
     public void clear() {
         this.username = null;
         this.inetAddress = null;
+        this.rememberMe = false;
 
         if( this.password != null ) {
             for( int i = 0; i < password.length; i++ ) {
@@ -245,6 +346,7 @@ public class UsernamePasswordToken implements InetAuthenticationToken {
         sb.append( getClass().getName() );
         sb.append( " - " );
         sb.append( username );
+        sb.append( ", rememberMe=" ).append( rememberMe );
         if ( inetAddress != null ) {
             sb.append( " (" ).append( inetAddress ).append( ")");
         }
