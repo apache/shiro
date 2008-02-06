50ea71cf6f632d275e45bd7d81bfec2fded11e5c
pharaohh <pharaoh@apache.org>
2008-02-06 06:17:20 +0000

    RememberMe support continued
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710674 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/jsecurity/support/DefaultPrincipalsConverter.java
@@ -0,0 +1,52 @@
+package org.jsecurity.support;
+
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.*;
+
+/**
+ *
+ */
+public class DefaultPrincipalsConverter implements PrincipalsConverter {
+
+    private static final String CHAR_ENCODING = "UTF8";
+
+    public String toString(Object principals) {
+        if ( principals == null ) {
+            String msg = "argument cannot be null.";
+            throw new IllegalArgumentException( msg );
+        }
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        XMLEncoder encoder = new XMLEncoder( new BufferedOutputStream( bos ) );
+        encoder.writeObject(principals);
+        encoder.close();
+
+        try {
+            return bos.toString( CHAR_ENCODING );
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException( "Unable to convert byte array to UTF-8.  " +
+                    "This is required to be supported on all 1.3+ JVMs.  Unable to continue" );
+        }
+    }
+
+    public Object fromString( String src ) {
+        if ( src == null ) {
+            String msg = "argument cannot be null.";
+            throw new IllegalArgumentException( msg );
+        }
+        ByteArrayInputStream bis = null;
+        try {
+            bis = new ByteArrayInputStream( src.getBytes( CHAR_ENCODING ) );
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException( "Unable to convert byte array to UTF-8.  " +
+                    "This is required to be supported on all 1.3+ JVMs.  Unable to continue" );
+        }
+
+
+        XMLDecoder decoder = new XMLDecoder( new BufferedInputStream( bis ) );
+        Object o = decoder.readObject();
+        decoder.close();
+        return o;
+    }
+}
