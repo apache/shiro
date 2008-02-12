4c8e3d8d2b3abb60d874dc6666478d4c5ff12eb7
pharaohh <dev-null@apache.org>
2008-02-12 01:51:42 +0000

    removed unnecessary package
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710695 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/jsecurity/cache/HashtableCacheProvider.java
@@ -0,0 +1,45 @@
+/*
+* Copyright (C) 2005-2007 Jeremy Haile, Les Hazlewood
+*
+* This library is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License as published
+* by the Free Software Foundation; either version 2.1 of the License, or
+* (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+* Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this library; if not, write to the
+*
+* Free Software Foundation, Inc.
+* 59 Temple Place, Suite 330
+* Boston, MA 02111-1307
+* USA
+*
+* Or, you may view it online at
+* http://www.opensource.org/licenses/lgpl-license.php
+*/
+package org.jsecurity.cache;
+
+/**
+ * A {@link CacheProvider} that returns {@link HashtableCache} caches.
+ *
+ * @since 0.2
+ * @author Jeremy Haile
+ * @author Les Hazlewood
+ */
+public class HashtableCacheProvider implements CacheProvider {
+
+    /**
+     * Builds a new {@link HashtableCache} with the given name.
+     *
+     * @param cacheName the name of the new cache to create.
+     * @return a new cache.
+     */
+    public Cache buildCache( String cacheName ) {
+        return new HashtableCache( cacheName );
+    }
+}
