f1d5fb5953a943daf62a23c7ff545f5adc69e649
pharaohh <pharaoh@apache.org>
2008-03-16 07:35:13 +0000

    JSEC-88 - converted to the Apache 2.0 license
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710804 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/jsecurity/codec/Base64.java
+++ src/org/jsecurity/codec/Base64.java
@@ -1,36 +1,11 @@
 /*
- * Copyright (C) 2005-2008 Les Hazlewood
+ * Copyright 2005-2008 Les Hazlewood and original authors
  *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
- * Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the
- *
- * Free Software Foundation, Inc.
- * 59 Temple Place, Suite 330
- * Boston, MA 02111-1307
- * USA
- *
- * Or, you may view it online at
- * http://www.opensource.org/licenses/lgpl-license.php
- *
- *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
--- test/org/jsecurity/web/WebSecurityManagerTest.java
+++ test/org/jsecurity/web/WebSecurityManagerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008 Les Hazlewood
+ * Copyright 2005-2008 Les Hazlewood
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
