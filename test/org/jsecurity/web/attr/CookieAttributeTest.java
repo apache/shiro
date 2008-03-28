42ce54e9a3302d5c16a09aed2a14d7c027bd3403
pharaohh <dev-null@apache.org>
2008-03-28 03:32:23 +0000

    updated license header
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710844 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/jsecurity/web/attr/CookieAttributeTest.java
+++ test/org/jsecurity/web/attr/CookieAttributeTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2005-2008 Les Hazlewood
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jsecurity.web.attr;
 
 import junit.framework.TestCase;
@@ -10,11 +25,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Created by IntelliJ IDEA.
- * User: lhazlewood
- * Date: Mar 27, 2008
- * Time: 10:35:45 PM
- * To change this template use File | Settings | File Templates.
+ * @since 0.9
+ * @author Les Hazlewood
  */
 public class CookieAttributeTest extends TestCase {
 
