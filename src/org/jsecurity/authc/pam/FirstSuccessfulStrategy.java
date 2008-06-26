79a7ddd27de555f5cf0102a90cadc74ce9386e6b
pharaohh <dev-null@apache.org>
2008-06-26 21:48:19 +0000

    JSEC-113 - initial implementation complete
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@710940 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/jsecurity/authc/pam/FirstSuccessfulStrategy.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2005-2008 Les Hazlewood
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jsecurity.authc.pam;
+
+import org.jsecurity.authc.Account;
+import org.jsecurity.authc.AuthenticationException;
+import org.jsecurity.authc.AuthenticationToken;
+import org.jsecurity.realm.Realm;
+
+import java.util.Collection;
+
+/**
+ * {@link ModularAuthenticationStrategy} implementation that only accepts the account data from
+ * the first successfully consulted Realm and ignores all subsequent realms.
+ *
+ * @author Les Hazlewood
+ * @since 0.9
+ */
+public class FirstSuccessfulStrategy extends AbstractAuthenticationStrategy {
+
+    public Account beforeAllAttempts(Collection<? extends Realm> realms, AuthenticationToken token) throws AuthenticationException {
+        return null;
+    }
+
+    public Account afterAttempt(Realm realm, AuthenticationToken token, Account singleRealmAccount, Account aggregateAccount, Throwable t) throws AuthenticationException {
+        if (aggregateAccount != null) {
+            //just keep the value, ignore all other realms
+            return aggregateAccount;
+        } else {
+            return singleRealmAccount;
+        }
+    }
+}
