d120707afe9578f8222bc6451358a8139e78bbe0
pharaohh <pharaoh@apache.org>
2008-07-23 21:05:57 +0000

    javadoc updates
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@711003 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/jsecurity/web/filter/AccessControlFilter.java
+++ src/org/jsecurity/web/filter/AccessControlFilter.java
@@ -35,7 +35,7 @@ import java.io.IOException;
  * @author Jeremy Haile
  * @since 0.9
  */
-public abstract class AccessControlFilter extends PathMatchingFilter{
+public abstract class AccessControlFilter extends PathMatchingFilter {
 
     // Key used when storing a SavedRequest in the session
     public static final String DEFAULT_LOGIN_URL = "/login.jsp";
@@ -49,6 +49,7 @@ public abstract class AccessControlFilter extends PathMatchingFilter{
 
     /**
      * Sets the login URL used when a user needs to be redirected for authentication.
+     *
      * @param loginUrl the login URL.
      */
     public void setLoginUrl(String loginUrl) {
@@ -60,27 +61,27 @@ public abstract class AccessControlFilter extends PathMatchingFilter{
      * proceed normally, or whether the request should be handled by the logic in
      * {@link #onAccessDenied(javax.servlet.ServletRequest, javax.servlet.ServletResponse)}.
      *
-     * @param request the request.
-     * @param response the response.
+     * @param request     the request.
+     * @param response    the response.
      * @param mappedValue the value mapped to this filter in the URL rules.
      * @return true if the reques should proceed normally, false if the request should be proceessed by this filter's
-     * {@link #onAccessDenied(javax.servlet.ServletRequest, javax.servlet.ServletResponse)} method.
+     *         {@link #onAccessDenied(javax.servlet.ServletRequest, javax.servlet.ServletResponse)} method.
      * @throws IOException if an error occurs during processing.
      */
     protected abstract boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception;
 
     /**
      * Template method sub-classes must implement. This method processes requests where the subject was denied access
-     * by the {@link #isAccessAllowed(javax.servlet.ServletRequest, javax.servlet.ServletResponse, Object)} method.
+     * by the {@link #isAccessAllowed(javax.servlet.ServletRequest, javax.servlet.ServletResponse, Object) isAccessAllowed}
+     * method.
      *
-     * @param request the servlet request.
+     * @param request  the servlet request.
      * @param response the servlet response.
      * @return true if the request should continue to be processed; false if the subclass will handle/render
      *         the response directly.
      * @throws Exception if there is an error processing the request.
      */
     protected abstract boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception;
-
 
     public boolean onPreHandle(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
         //mapped value is ignored - not needed for most (if not all) authc Filters.
