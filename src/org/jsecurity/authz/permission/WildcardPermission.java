009302ff9da9348bdb83f4b9267d8143b4350f49
pharaohh <dev-null@apache.org>
2008-07-23 22:04:59 +0000

    javadoc updates
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@711004 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/jsecurity/authz/permission/WildcardPermission.java
+++ src/org/jsecurity/authz/permission/WildcardPermission.java
@@ -28,7 +28,80 @@ import java.util.List;
 import java.util.Set;
 
 /**
+ * A <code>WildcardPermission</code> is a very flexible permission construct supporting multiple levels of
+ * permission matching. However, most people will probably follow some standard conventions as explained below.
+ *
+ * <h3>Simple Usage</h3>
+ *
+ * In the simplest form, <code>WildcardPermission</code> can be used as a simple permission string. You could grant a
+ * user an &quot;editNewsletter&quot; permission and then check to see if the user has the editNewsletter
+ * permission by calling
+ * <p/>
+ * <code>subject.isPermitted(&quot;editNewsletter&quot;)</code>
+ * <p/>
+ * This is (mostly) equivalent to
+ * <p/>
+ * <code>subject.isPermitted( new WildcardPermission(&quot;editNewsletter&quot;) )</code>
+ * <p/>
+ * but more on that later.
+ * <p/>
+ * The simple permission string may work for simple applications, but it requires you to have permissions like
+ * <code>&quot;viewNewsletter&quot;</code>, <code>&quot;deleteNewsletter&quot;</code>,
+ * <code>&quot;createNewsletter&quot;</code>, etc. You can also grant a user <code>&quot;*&quot;</code> permissions
+ * using the wildcard character (giving this class its name), which means they have <em>all</em> permissions. But
+ * using this approach there's no way to just say a user has &quot;all newsletter permissions&quot;.
+ * <p/>
+ * For this reason, <code>WildcardPermission</code> supports multiple <em>levels</em> of permissioning.
+ *
+ * <h3>Multiple Levels</h3>
+ *
+ * WildcardPermission</code> also supports the concept of multiple <em>levels</em>.  For example, you could
+ * restructure the previous simple example by granting a user the permission <code>&quot;newsletter:edit&quot;</code>.
+ * The colon in this example is a special character used by the <code>WildcardPermission</code> that delimits the
+ * next token in the permission.
+ * <p/>
+ * In this example, the first token is the <em>domain</em> that is being operated on
+ * and the second token is the <em>action</em> being performed. Each level can contain multiple values.  So you
+ * could simply grant a user the permission <code>&quot;newsletter:view,edit,create&quot;</code> which gives them
+ * access to perform <code>view</code>, <code>edit</code>, and <code>create</code> actions in the <code>newsletter</code>
+ * <em>domain</em>. Then you could check to see if the user has the <code>&quot;newsletter:create&quot;</code>
+ * permission by calling
+ * <p/>
+ * <code>subject.isPermitted(&quot;newsletter:create&quot;)</code>
+ * <p/>
+ * (which would return true).
+ * <p/>
+ * In addition to granting multiple permissions via a single string, you can grant all permission for a particular
+ * level. So if you wanted to grant a user all actions in the <code>newsletter</code> domain, you could simply give
+ * them <code>&quot;newsletter:*&quot;</code>. Now, any permission check for <code>&quot;newsletter:XXX&quot;</code>
+ * will return <code>true</code>. It is also possible to use the wildcard token at the domain level (or both): so you
+ * could grant a user the <code>&quot;view&quot;</code> action across all domains <code>&quot;*:view&quot;</code>.
+ *
+ * <h3>Instance-level Access Control</h3>
+ *
+ * Another common usage of the <code>WildcardPermission</code> is to model instance-level Access Control Lists.
+ * In this scenario you use three tokens - the first is the <em>domain</em>, the second is the <em>action</em>, and
+ * the third is the <em>instance</em> you are acting on.
+ * <p/>
+ * So for example you could grant a user <code>&quot;newsletter:edit:12,13,18&quot;</code>.  In this example, assume
+ * that the third token is the system's ID of the newsletter. That would allow the user to edit newsletters
+ * <code>12</code>, <code>13</code>, and <code>18</code>. This is an extremely powerful way to express permissions,
+ * since you can now say things like <code>&quot;newsletter:*:13&quot;</code> (grant a user all actions for newsletter
+ * <code>13</code>), <code>&quot;newsletter:view,create,edit:*&quot;</code> (allow the user to
+ * <code>view</code>, <code>create</code>, or <code>edit</code> <em>any</em> newsletter), or
+ * <code>&quot;newsletter:*:*</code> (allow the user to perform <em>any</em> action on <em>any</em> newsletter).
+ * <p/>
+ * To perform checks against these instance-level permissions, the application should include the instance ID in the
+ * permission check like so:
+ * <p/>
+ * <code>subject.isPermitted( &quot;newsletter:edit:13&quot; )</code>
+ * <p/>
+ * There is no limit to the number of tokens that can be used, so it is up to your imagination in terms of ways that
+ * this could be used in your application.  However, the JSecurity team likes to standardize some common usages shown
+ * above to help people get started and provide consistency in the JSecurity community.
+ *
  * @author Jeremy Haile
+ * @author Les Hazlewood
  * @since 0.9
  */
 public class WildcardPermission implements Permission, Serializable {
