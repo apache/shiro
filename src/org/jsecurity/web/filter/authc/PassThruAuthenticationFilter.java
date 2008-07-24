8e69b634c800e69e37024b7151eee7f37d5d6722
mentiro <dev-null@apache.org>
2008-07-24 21:57:41 +0000

    Updated pass thru javadoc.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@711014 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/jsecurity/web/filter/authc/PassThruAuthenticationFilter.java
+++ src/org/jsecurity/web/filter/authc/PassThruAuthenticationFilter.java
@@ -25,9 +25,22 @@ import javax.servlet.ServletResponse;
  * An authentication filter that redirects the user to the login page when they are trying to access
  * a protected resource.  However, if the user is trying to access the login page, the filter lets
  * the request pass through to the application code.
+ * <p/>
+ * The difference between this controller and the {@link FormAuthenticationFilter} is that on a login submission
+ * (by default an HTTP POST to the login URL), the form controller attempts to automatically authenticate the
+ * user by passing the "username" and "password" request parameter values to
+ * {@link org.jsecurity.subject.Subject#login(org.jsecurity.authc.AuthenticationToken)}.
+ * <p/>
+ * This controller always passes all requests to the login URL through, both GETs and POSTs.
+ * This is useful in cases where the developer
+ * wants to write their own login behavior, which should include a call to
+ * {@link org.jsecurity.subject.Subject#login(org.jsecurity.authc.AuthenticationToken)} at some point.  For example,
+ * if the developer has a login controller or validator with custom login behavior, this controller may be
+ * appropriate.
  *
  * @author Jeremy Haile
  * @since 0.9
+ * @see FormAuthenticationFilter
  */
 public class PassThruAuthenticationFilter extends AuthenticationFilter {
 
