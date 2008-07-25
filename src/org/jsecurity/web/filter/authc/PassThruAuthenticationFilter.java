d11a97449361a2394c5168ec555afe74c3aab5b2
pharaohh <pharaoh@apache.org>
2008-07-25 16:21:51 +0000

    javadoc updates
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@711016 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/jsecurity/web/filter/authc/PassThruAuthenticationFilter.java
+++ src/org/jsecurity/web/filter/authc/PassThruAuthenticationFilter.java
@@ -26,21 +26,21 @@ import javax.servlet.ServletResponse;
  * a protected resource.  However, if the user is trying to access the login page, the filter lets
  * the request pass through to the application code.
  * <p/>
- * The difference between this controller and the {@link FormAuthenticationFilter} is that on a login submission
- * (by default an HTTP POST to the login URL), the form controller attempts to automatically authenticate the
- * user by passing the "username" and "password" request parameter values to
- * {@link org.jsecurity.subject.Subject#login(org.jsecurity.authc.AuthenticationToken)}.
+ * The difference between this filter and the {@link FormAuthenticationFilter FormAuthenticationFilter} is that
+ * on a login submission (by default an HTTP POST to the login URL), the <code>FormAuthenticationFilter</code> filter
+ * attempts to automatically authenticate the user by passing the <code>username</code> and <code>password</code>
+ * request parameter values to
+ * {@link org.jsecurity.subject.Subject#login(org.jsecurity.authc.AuthenticationToken) Subject.login(usernamePasswordToken)}.
  * <p/>
- * This controller always passes all requests to the {@link #loginUrl} through, both GETs and POSTs.
- * This is useful in cases where the developer
- * wants to write their own login behavior, which should include a call to
- * {@link org.jsecurity.subject.Subject#login(org.jsecurity.authc.AuthenticationToken)} at some point.  For example,
- * if the developer has a login controller or validator with custom login behavior, this controller may be
- * appropriate.
+ * Conversely, this controller always passes all requests to the {@link #setLoginUrl loginUrl} through, both GETs and
+ * POSTs.  This is useful in cases where the developer wants to write their own login behavior, which should include a
+ * call to {@link org.jsecurity.subject.Subject#login(org.jsecurity.authc.AuthenticationToken) Subject.login(AuthenticationToken)}
+ * at some point.  For example,  if the developer has their own custom MVC login controller or validator,
+ * this <code>PassThruAuthenticationFilter</code> may be appropriate.
  *
  * @author Jeremy Haile
- * @since 0.9
  * @see FormAuthenticationFilter
+ * @since 0.9
  */
 public class PassThruAuthenticationFilter extends AuthenticationFilter {
 
