ce62cb874a470aeacebd6ce2051fd2d5d3e88f1a
Alan Cabrera <adc@apache.org>
2009-03-11 06:34:07 +0000

    KI-66 intermediate changes
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@752389 13f79535-47bb-0310-9956-ffa450edef68

--- support/quartz/src/org/apache/ki/session/mgt/quartz/QuartzSessionValidationJob.java
+++ support/quartz/src/org/apache/ki/session/mgt/quartz/QuartzSessionValidationJob.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.ki.session.mgt.quartz;
+package org.apache.ki.session.mgt.quartz;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
