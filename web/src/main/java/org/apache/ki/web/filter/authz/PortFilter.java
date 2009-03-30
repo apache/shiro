998fe8a53866bb1fc8d6af15b980ff7b7e605ea0
Les Hazlewood <lhazlewood@apache.org>
2009-03-30 16:57:59 +0000

    added Apache license and @since information
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jsecurity/trunk@760044 13f79535-47bb-0310-9956-ffa450edef68

--- web/src/main/java/org/apache/ki/web/filter/authz/PortFilter.java
+++ web/src/main/java/org/apache/ki/web/filter/authz/PortFilter.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.ki.web.filter.authz;
 
 import org.apache.ki.config.ConfigurationException;
@@ -13,7 +31,7 @@ import java.io.IOException;
  * A Filter that requires the request to be on a specific port, and if not, redirects to the same URL on that port.
  *
  * @author Les Hazlewood
- * @since Mar 30, 2009 10:46:21 AM
+ * @since 1.0
  */
 public class PortFilter extends AuthorizationFilter {
 
