2a0f133b7c88f0864c99606e7f2451ac0a1d07bf
Les Hazlewood <lhazlewood@apache.org>
2009-07-18 15:43:12 +0000

    Added another test case for sessionTimeout
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shiro/trunk@795387 13f79535-47bb-0310-9956-ffa450edef68

--- web/src/test/java/org/apache/shiro/web/DefaultWebSecurityManagerTest.java
+++ web/src/test/java/org/apache/shiro/web/DefaultWebSecurityManagerTest.java
@@ -19,9 +19,22 @@
 package org.apache.shiro.web;
 
 import org.apache.shiro.util.ThreadContext;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.mgt.AbstractSessionManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.easymock.EasyMock.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.Serializable;
 
 /**
  * @author Les Hazlewood
@@ -46,6 +59,45 @@ public class DefaultWebSecurityManagerTest {
     @Test
     public void shiroSessionModeInit() {
         sm.setSessionMode(DefaultWebSecurityManager.NATIVE_SESSION_MODE);
+    }
+
+    protected void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Test
+    public void testSessionTimeout() {
+        shiroSessionModeInit();
+        long globalTimeout = 50;
+        sm.setGlobalSessionTimeout(globalTimeout);
+
+        HttpServletRequest mockRequest = createNiceMock(HttpServletRequest.class);
+        WebUtils.bind(mockRequest);
+        HttpServletResponse mockResponse = createNiceMock(HttpServletResponse.class);
+        WebUtils.bind(mockResponse);
+
+        expect(mockRequest.getCookies()).andReturn(null);
+        expect(mockRequest.getContextPath()).andReturn("/");
+
+        replay(mockRequest);
+
+        Subject subject = sm.getSubject();
+        Session session = subject.getSession();
+        Serializable origId = session.getId();
+        assertEquals(session.getTimeout(), globalTimeout);
+        session.setTimeout(100);
+        assertEquals(session.getTimeout(), 100);
+        sleep(100);
+        //now the underlying session should have been expired and a new one replaced by default.
+        //so ensure the replaced session has the default session timeout:
+        assertEquals(session.getTimeout(), globalTimeout);
+        assertFalse(origId.equals(session.getId())); //new ID would have been generated
+
+        //verify(mockRequest);
     }
 
 }
