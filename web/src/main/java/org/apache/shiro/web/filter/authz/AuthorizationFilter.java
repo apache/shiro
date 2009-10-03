61c15427f6b1204706b3d10131efda813043e3cf
Kalle Korhonen <kaosko@apache.org>
2009-10-03 05:18:09 +0000

    Fix issue SHIRO-106, call sendError instead of setStatus for setting 401 to invoke container's ERROR dispatcher
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shiro/trunk@821251 13f79535-47bb-0310-9956-ffa450edef68

--- web/src/main/java/org/apache/shiro/web/filter/authz/AuthorizationFilter.java
+++ web/src/main/java/org/apache/shiro/web/filter/authz/AuthorizationFilter.java
@@ -115,7 +115,7 @@ public abstract class AuthorizationFilter extends AccessControlFilter {
         } else {
             // If subject is known but not authorized, redirect to the unauthorized URL if there is one
             // If no unauthorized URL is specified, just return an unauthorized HTTP status code
-            WebUtils.toHttp(response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            WebUtils.toHttp(response).sendError(HttpServletResponse.SC_UNAUTHORIZED);
             if (StringUtils.hasText(getUnauthorizedUrl())) {
                 WebUtils.issueRedirect(request, response, getUnauthorizedUrl());
             }
