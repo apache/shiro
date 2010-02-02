bd2244b2ea3c40f596e0f51d6a3e1c586e0bf2cf
Les Hazlewood <lhazlewood@apache.org>
2010-02-02 20:37:50 +0000

    Added the ASF license header where necessary
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shiro/trunk@905784 13f79535-47bb-0310-9956-ffa450edef68

--- samples/aspectj/src/main/java/org/apache/shiro/sample/bank/AccountTransaction.java
+++ samples/aspectj/src/main/java/org/apache/shiro/sample/bank/AccountTransaction.java
@@ -1,126 +1,144 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.shiro.sample.bank;
-
-
-import java.sql.Timestamp;
-import java.util.Date;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class AccountTransaction {
 
-  private static long _SEQUENCE;
-  
-  public enum TransactionType {
-    DEPOSIT,
-    WITHDRAWAL;
-  }
-  
-  private long _id;
-  
-  private TransactionType _type;
-  
-  private long _accountId;
-  
-  private double _amount;
-  
-  private String _createdBy;
-  private Date _creationDate;
-  
-  public static AccountTransaction createDepositTx(long anAccountId, double anAmount) {
-    AccountTransaction tx = new AccountTransaction(TransactionType.DEPOSIT, anAccountId, anAmount);
-    return tx;
-  }
-  
-  public static AccountTransaction createWithdrawalTx(long anAccountId, double anAmount) {
-    AccountTransaction tx = new AccountTransaction(TransactionType.WITHDRAWAL, anAccountId, anAmount);
-    return tx;
-  }
-  
-  private AccountTransaction(TransactionType aType, long anAccountId, double anAmount) {
-    _id = ++_SEQUENCE;
-    _type = aType;
-    _accountId = anAccountId;
-    _amount = anAmount;
-    _createdBy = "unknown";
-    _creationDate = new Date();
-  }
+    private static long _SEQUENCE;
 
-  /**
-   * Returns the id attribute.
-   *
-   * @return The id value.
-   */
-  public long getId() {
-    return _id;
-  }
+    public enum TransactionType {
+        DEPOSIT,
+        WITHDRAWAL;
+    }
 
-  /**
-   * Returns the type attribute.
-   *
-   * @return The type value.
-   */
-  public TransactionType getType() {
-    return _type;
-  }
+    private long _id;
 
-  /**
-   * Returns the accountId attribute.
-   *
-   * @return The accountId value.
-   */
-  public long getAccountId() {
-    return _accountId;
-  }
+    private TransactionType _type;
 
-  /**
-   * Returns the amount attribute.
-   *
-   * @return The amount value.
-   */
-  public double getAmount() {
-    return _amount;
-  }
-  
-  /**
-   * Changes the value of the attributes createdBy.
-   *
-   * @param aCreatedBy The new value of the createdBy attribute.
-   */
-  protected void setCreatedBy(String aCreatedBy) {
-    _createdBy = aCreatedBy;
-  }
+    private long _accountId;
 
-  /**
-   * Returns the createdBy attribute.
-   *
-   * @return The createdBy value.
-   */
-  public String getCreatedBy() {
-    return _createdBy;
-  }
+    private double _amount;
 
-  /**
-   * Returns the creationDate attribute.
-   *
-   * @return The creationDate value.
-   */
-  public Date getCreationDate() {
-    return _creationDate;
-  }
-  
-  /* (non-Javadoc)
-   * @see java.lang.Object#toString()
-   */
-  public String toString() {
-    return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).
-            append("id", _id).
-            append("type", _type).
-            append("accountId", _accountId).
-            append("amount", _amount).
-            append("createdBy", _createdBy).
-            append("creationDate", new Timestamp(_creationDate.getTime())).
-            toString();
-  }
-  
+    private String _createdBy;
+    private Date _creationDate;
+
+    public static AccountTransaction createDepositTx(long anAccountId, double anAmount) {
+        AccountTransaction tx = new AccountTransaction(TransactionType.DEPOSIT, anAccountId, anAmount);
+        return tx;
+    }
+
+    public static AccountTransaction createWithdrawalTx(long anAccountId, double anAmount) {
+        AccountTransaction tx = new AccountTransaction(TransactionType.WITHDRAWAL, anAccountId, anAmount);
+        return tx;
+    }
+
+    private AccountTransaction(TransactionType aType, long anAccountId, double anAmount) {
+        _id = ++_SEQUENCE;
+        _type = aType;
+        _accountId = anAccountId;
+        _amount = anAmount;
+        _createdBy = "unknown";
+        _creationDate = new Date();
+    }
+
+    /**
+     * Returns the id attribute.
+     *
+     * @return The id value.
+     */
+    public long getId() {
+        return _id;
+    }
+
+    /**
+     * Returns the type attribute.
+     *
+     * @return The type value.
+     */
+    public TransactionType getType() {
+        return _type;
+    }
+
+    /**
+     * Returns the accountId attribute.
+     *
+     * @return The accountId value.
+     */
+    public long getAccountId() {
+        return _accountId;
+    }
+
+    /**
+     * Returns the amount attribute.
+     *
+     * @return The amount value.
+     */
+    public double getAmount() {
+        return _amount;
+    }
+
+    /**
+     * Changes the value of the attributes createdBy.
+     *
+     * @param aCreatedBy The new value of the createdBy attribute.
+     */
+    protected void setCreatedBy(String aCreatedBy) {
+        _createdBy = aCreatedBy;
+    }
+
+    /**
+     * Returns the createdBy attribute.
+     *
+     * @return The createdBy value.
+     */
+    public String getCreatedBy() {
+        return _createdBy;
+    }
+
+    /**
+     * Returns the creationDate attribute.
+     *
+     * @return The creationDate value.
+     */
+    public Date getCreationDate() {
+        return _creationDate;
+    }
+
+    /* (non-Javadoc)
+    * @see java.lang.Object#toString()
+    */
+
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).
+                append("id", _id).
+                append("type", _type).
+                append("accountId", _accountId).
+                append("amount", _amount).
+                append("createdBy", _createdBy).
+                append("creationDate", new Timestamp(_creationDate.getTime())).
+                toString();
+    }
+
 }
