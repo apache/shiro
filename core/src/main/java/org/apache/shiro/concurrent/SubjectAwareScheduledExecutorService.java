d020bda9ff33a334acb406c59aa588d463c63d2b
Les Hazlewood <lhazlewood@apache.org>
2010-02-19 23:02:12 +0000

    SHIRO-140 - added initial implementations with basic tests.  More tests to come over the weekend.  Need to document this in the wiki too.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shiro/trunk@912025 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/main/java/org/apache/shiro/concurrent/SubjectAwareScheduledExecutorService.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.shiro.concurrent;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Same concept as the {@link SubjectAwareExecutorService} but additionally supports the
+ * {@link ScheduledExecutorService} interface.
+ */
+public class SubjectAwareScheduledExecutorService extends SubjectAwareExecutorService implements ScheduledExecutorService {
+
+    private ScheduledExecutorService targetScheduledExecutorService;
+
+    public SubjectAwareScheduledExecutorService() {
+    }
+
+    public SubjectAwareScheduledExecutorService(ScheduledExecutorService target) {
+        setTargetScheduledExecutorService(target);
+    }
+
+    public ScheduledExecutorService getTargetScheduledExecutorService() {
+        return targetScheduledExecutorService;
+    }
+
+    public void setTargetScheduledExecutorService(ScheduledExecutorService targetScheduledExecutorService) {
+        this.targetScheduledExecutorService = targetScheduledExecutorService;
+        setTargetExecutorService(targetScheduledExecutorService);
+    }
+
+    public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
+        Runnable work = associateWithSubject(command);
+        return this.targetScheduledExecutorService.schedule(work, delay, unit);
+    }
+
+    public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit) {
+        Callable<V> work = associateWithSubject(callable);
+        return this.targetScheduledExecutorService.schedule(work, delay, unit);
+    }
+
+    public ScheduledFuture<?> scheduleAtFixedRate(Runnable command, long initialDelay, long period, TimeUnit unit) {
+        Runnable work = associateWithSubject(command);
+        return this.targetScheduledExecutorService.scheduleAtFixedRate(work, initialDelay, period, unit);
+    }
+
+    public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit) {
+        Runnable work = associateWithSubject(command);
+        return this.targetScheduledExecutorService.scheduleWithFixedDelay(work, initialDelay, delay, unit);
+    }
+}
---
+++ core/src/test/java/org/apache/shiro/test/SecurityManagerTestSupport.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.shiro.test;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.config.Ini;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.realm.text.IniRealm;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.LifecycleUtils;
+import org.apache.shiro.util.ThreadContext;
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * Utility methods for use by Shiro test case subclasses.  You can use these methods as examples for your own
+ * test cases, but you SHOULD NOT use any ThreadContext API calls in your actual application code.
+ * The utility methods here make heavy assumptions about Shiro's implementation details, and your
+ * application code should definitely not.
+ * <p/>
+ * See the <a href="http://cwiki.apache.org/confluence/display/SHIRO/Subject">wiki Subject documentation</a>
+ * for proper application practices using Subject instances with threads.
+ */
+public class SecurityManagerTestSupport {
+
+    protected SecurityManager createTestSecurityManager() {
+        Ini ini = new Ini();
+        ini.setSectionProperty("users", "test", "test");
+        return new DefaultSecurityManager(new IniRealm(ini));
+    }
+
+    protected void destroy(SecurityManager sm) {
+        LifecycleUtils.destroy(sm);
+    }
+
+    protected SecurityManager createAndBindTestSecurityManager() {
+        SecurityManager sm = createTestSecurityManager();
+        ThreadContext.bind(sm);
+        return sm;
+    }
+
+    protected void unbindAndDestroyTestSecurityManager() {
+        SecurityManager sm = ThreadContext.unbindSecurityManager();
+        if (sm != null) {
+            destroy(sm);
+        }
+    }
+
+    protected Subject createAndBindTestSubject() {
+        SecurityManager sm = ThreadContext.getSecurityManager();
+        if (sm == null) {
+            createAndBindTestSecurityManager();
+        }
+        return SecurityUtils.getSubject();
+    }
+
+    protected void unbindTestSubject() {
+        ThreadContext.unbindSubject();
+    }
+
+    @Before
+    public void setup() {
+        createAndBindTestSubject();
+    }
+
+    @After
+    public void teardown() {
+        unbindTestSubject();
+        unbindAndDestroyTestSecurityManager();
+    }
+}
