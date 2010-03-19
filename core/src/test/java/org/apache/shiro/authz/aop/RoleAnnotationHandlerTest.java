852d9a477326e554d67c82bc6833707c3a41154f
Les Hazlewood <lhazlewood@apache.org>
2010-03-19 02:04:44 +0000

    SHIRO-146: - Committed changes PermissionAnnotationHandler and RoleAnnotationHandler to ensure that Permission/Role assertions throw an UnauthenticatedException if the user isn't authenticated or remembered.  Accompanied by 2 test classes. - Reformatted DummyTestService to use standard coding conventions so we can easily determine real changes - Slightly changed the DelegatingSubject UnauthenticatedException message to be slightly less verbose
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shiro/trunk@925078 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/test/java/org/apache/shiro/authz/aop/RoleAnnotationHandlerTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2008 Les Hazlewood
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.shiro.authz.aop;
+
+import org.apache.shiro.authz.UnauthenticatedException;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.apache.shiro.test.SecurityManagerTestSupport;
+import org.junit.Test;
+
+import java.lang.annotation.Annotation;
+
+/**
+ * Test cases for the {@link RoleAnnotationHandler} class.
+ */
+public class RoleAnnotationHandlerTest extends SecurityManagerTestSupport {
+
+    @Test(expected = UnauthenticatedException.class)
+    public void testGuestSingleRoleAssertion() throws Throwable {
+        RoleAnnotationHandler handler = new RoleAnnotationHandler();
+
+        Annotation requiresRolesAnnotation = new RequiresRoles() {
+            public String value() {
+                return "blah";
+            }
+
+            public Class<? extends Annotation> annotationType() {
+                return RequiresRoles.class;
+            }
+        };
+
+        handler.assertAuthorized(requiresRolesAnnotation);
+    }
+
+    @Test(expected = UnauthenticatedException.class)
+    public void testGuestMultipleRolesAssertion() throws Throwable {
+        RoleAnnotationHandler handler = new RoleAnnotationHandler();
+
+        Annotation requiresRolesAnnotation = new RequiresRoles() {
+            public String value() {
+                return "blah, blah2";
+            }
+
+            public Class<? extends Annotation> annotationType() {
+                return RequiresRoles.class;
+            }
+        };
+
+        handler.assertAuthorized(requiresRolesAnnotation);
+    }
+}
