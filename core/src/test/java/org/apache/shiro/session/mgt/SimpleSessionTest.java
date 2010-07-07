59c0a7dedc40a303485effff559c705ed631212d
Les Hazlewood <lhazlewood@apache.org>
2010-07-07 23:23:34 +0000

    Added ASF header
    git-svn-id: https://svn.apache.org/repos/asf/incubator/shiro/trunk@961533 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/shiro/session/mgt/SimpleSessionTest.java
+++ core/src/test/java/org/apache/shiro/session/mgt/SimpleSessionTest.java
@@ -1,35 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.shiro.session.mgt;
+
+import org.junit.Test;
+
+import java.io.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
-import org.junit.Test;
-
 public class SimpleSessionTest {
     @Test
     public void serializeHost() throws IOException, ClassNotFoundException {
-	SimpleSession session = new SimpleSession("localhost");
-	assertEquals("localhost", serializeAndDeserialize(session).getHost());
+        SimpleSession session = new SimpleSession("localhost");
+        assertEquals("localhost", serializeAndDeserialize(session).getHost());
     }
-    
+
     @Test
     public void serializeExpired() throws IOException, ClassNotFoundException {
-	SimpleSession session = new SimpleSession();
-	session.setExpired(true);
-	assertTrue(serializeAndDeserialize(session).isExpired());
+        SimpleSession session = new SimpleSession();
+        session.setExpired(true);
+        assertTrue(serializeAndDeserialize(session).isExpired());
     }
-    
+
     private SimpleSession serializeAndDeserialize(SimpleSession session) throws IOException, ClassNotFoundException {
-	ByteArrayOutputStream serialized = new ByteArrayOutputStream();
-	ObjectOutputStream serializer = new ObjectOutputStream(serialized);
-	serializer.writeObject(session);
-	serializer.close();
-	return (SimpleSession) new ObjectInputStream(new ByteArrayInputStream(serialized.toByteArray())).readObject(); 	
+        ByteArrayOutputStream serialized = new ByteArrayOutputStream();
+        ObjectOutputStream serializer = new ObjectOutputStream(serialized);
+        serializer.writeObject(session);
+        serializer.close();
+        return (SimpleSession) new ObjectInputStream(new ByteArrayInputStream(serialized.toByteArray())).readObject();
     }
 }
