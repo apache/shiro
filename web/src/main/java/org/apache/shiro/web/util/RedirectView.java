73b9ceb4cddde9135e5066381e5ea5f73d3f4c58
Kalle Korhonen <kaosko@apache.org>
2010-10-12 04:22:32 +0000

    RESOLVED - issue SHIRO-191: Change all StringBuffer usages to StringBuilder https://issues.apache.org/jira/browse/SHIRO-191 - Fixed the only remaining use of StringBuffer (Javadoc stated the fix is for 2.x and comments talk about JDK 1.3 compatibility which is likely not relevant anymore)
    git-svn-id: https://svn.apache.org/repos/asf/shiro/trunk@1021622 13f79535-47bb-0310-9956-ffa450edef68

--- web/src/main/java/org/apache/shiro/web/util/RedirectView.java
+++ web/src/main/java/org/apache/shiro/web/util/RedirectView.java
@@ -191,7 +191,7 @@ public class RedirectView {
             Map model, HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         // Prepare name URL.
-        StringBuffer targetUrl = new StringBuffer();
+        StringBuilder targetUrl = new StringBuilder();
         if (this.contextRelative && getUrl().startsWith("/")) {
             // Do not apply context path to relative URLs.
             targetUrl.append(request.getContextPath());
@@ -214,12 +214,8 @@ public class RedirectView {
      * @see #urlEncode
      * @see #queryProperties
      * @see #urlEncode(String, String)
-     * @deprecated this method accepts a less-than-ideal StringBuffer argument and will be replaced by
-     * a StringBuilder argument in the next major version (2.x) of Shiro.  It remains in place to retain
-     * 1.0 -> 1.1 backwards compatibility.  See <a href="https://issues.apache.org/jira/browse/SHIRO-196">SHIRO-196</a>.
      */
-    @Deprecated
-    protected void appendQueryProperties(StringBuffer targetUrl, Map model, String encodingScheme)
+    protected void appendQueryProperties(StringBuilder targetUrl, Map model, String encodingScheme)
             throws UnsupportedEncodingException {
 
         // Extract anchor fragment, if any.
