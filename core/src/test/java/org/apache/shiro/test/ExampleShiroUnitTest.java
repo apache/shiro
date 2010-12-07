b39c291369779d071a87de217fa6dd70ef944a2b
Les Hazlewood <lhazlewood@apache.org>
2010-12-07 19:06:41 +0000

    Updated example test classes to reflect updated documentation.
    git-svn-id: https://svn.apache.org/repos/asf/shiro/trunk@1043165 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/test/java/org/apache/shiro/test/ExampleShiroUnitTest.java
@@ -0,0 +1,45 @@
+package org.apache.shiro.test;
+
+import org.apache.shiro.subject.Subject;
+import org.junit.After;
+import org.junit.Test;
+
+import static org.easymock.EasyMock.createNiceMock;
+import static org.easymock.EasyMock.expect;
+
+/**
+ * Simple example test class showing how one may perform unit tests for code that requires Shiro APIs.
+ */
+public class ExampleShiroUnitTest extends AbstractShiroTest {
+
+    @Test
+    public void testSimple() {
+
+        //1.  Create a mock Subject instance for the test to run
+        //    (for example, as an authenticated Subject):
+        Subject subjectUnderTest = createNiceMock(Subject.class);
+        expect(subjectUnderTest.isAuthenticated()).andReturn(true);
+
+        //2. Bind the subject to the current thread:
+        setSubject(subjectUnderTest);
+
+        //perform test logic here.  Any call to
+        //SecurityUtils.getSubject() directly (or nested in the
+        //call stack) will work properly.
+    }
+
+    /**
+     * Method is optional - use it with the @After annotation if you want
+     * to guarantee that the Subject is removed after every test method.
+     * You'll just need to set up a new Subject instance for every test
+     * that requires the Subject to be present.
+     * tests
+     * that require the Subject to be present.
+     */
+    @After
+    public void tearDownSubject() {
+        //3. Unbind the subject from the current thread:
+        clearSubject();
+    }
+
+}
