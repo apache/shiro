3d17c968e2842dacc9a3f1ce30fafe33de0a2d67
Les Hazlewood <lhazlewood@apache.org>
2011-04-08 18:54:16 +0000

    SHIRO-282: added ByteSource.Util inner class and changed direct SimpleByteSource usages to use ByteSource.Util instead.
    git-svn-id: https://svn.apache.org/repos/asf/shiro/trunk@1090400 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/shiro/util/ByteSource.java
+++ core/src/main/java/org/apache/shiro/util/ByteSource.java
@@ -18,12 +18,14 @@
  */
 package org.apache.shiro.util;
 
+import java.io.File;
+import java.io.InputStream;
+
 /**
- * A {@code ByteSource} wraps a byte array and provides additional encoding operations.  Most users will find
- * that the {@link SimpleByteSource SimpleByteSource} implementation meets most needs.
+ * A {@code ByteSource} wraps a byte array and provides additional encoding operations.  Most users will find the
+ * {@link Util} inner class sufficient to construct ByteSource instances.
  *
  * @since 1.0
- * @see SimpleByteSource
  */
 public interface ByteSource {
 
@@ -51,4 +53,75 @@ public interface ByteSource {
      *         underlying wrapped byte array.
      */
     public String toBase64();
+
+    /**
+     * Utility class that can construct ByteSource instances.  This is slightly nicer than needing to know the
+     * {@code ByteSource} implementation class to use.
+     *
+     * @since 1.2
+     */
+    public static final class Util {
+
+        /**
+         * Returns a new {@code ByteSource} instance representing the specified byte array.
+         *
+         * @param bytes the bytes to represent as a {@code ByteSource} instance.
+         * @return a new {@code ByteSource} instance representing the specified byte array.
+         */
+        public static ByteSource bytes(byte[] bytes) {
+            return new SimpleByteSource(bytes);
+        }
+
+        /**
+         * Returns a new {@code ByteSource} instance representing the specified character array's bytes.  The byte
+         * array is obtained assuming {@code UTF-8} encoding.
+         *
+         * @param chars the character array to represent as a {@code ByteSource} instance.
+         * @return a new {@code ByteSource} instance representing the specified character array's bytes.
+         */
+        public static ByteSource bytes(char[] chars) {
+            return new SimpleByteSource(chars);
+        }
+
+        /**
+         * Returns a new {@code ByteSource} instance representing the specified string's bytes.  The byte
+         * array is obtained assuming {@code UTF-8} encoding.
+         *
+         * @param string the string to represent as a {@code ByteSource} instance.
+         * @return a new {@code ByteSource} instance representing the specified string's bytes.
+         */
+        public static ByteSource bytes(String string) {
+            return new SimpleByteSource(string);
+        }
+
+        /**
+         * Returns a new {@code ByteSource} instance representing the specified ByteSource.
+         *
+         * @param source the ByteSource to represent as a new {@code ByteSource} instance.
+         * @return a new {@code ByteSource} instance representing the specified ByteSource.
+         */
+        public static ByteSource bytes(ByteSource source) {
+            return new SimpleByteSource(source);
+        }
+
+        /**
+         * Returns a new {@code ByteSource} instance representing the specified File's bytes.
+         *
+         * @param file the file to represent as a {@code ByteSource} instance.
+         * @return a new {@code ByteSource} instance representing the specified File's bytes.
+         */
+        public static ByteSource bytes(File file) {
+            return new SimpleByteSource(file);
+        }
+
+        /**
+         * Returns a new {@code ByteSource} instance representing the specified InputStream's bytes.
+         *
+         * @param stream the InputStream to represent as a {@code ByteSource} instance.
+         * @return a new {@code ByteSource} instance representing the specified InputStream's bytes.
+         */
+        public static ByteSource bytes(InputStream stream) {
+            return new SimpleByteSource(stream);
+        }
+    }
 }
