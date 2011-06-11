30bf035fa35113a3f90c6721b0fa4e109f983782
Les Hazlewood <lhazlewood@apache.org>
2011-06-11 21:02:14 +0000

    Minor JavaDoc fix
    git-svn-id: https://svn.apache.org/repos/asf/shiro/trunk@1134755 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/shiro/crypto/RandomNumberGenerator.java
+++ core/src/main/java/org/apache/shiro/crypto/RandomNumberGenerator.java
@@ -32,12 +32,11 @@ import org.apache.shiro.util.ByteSource;
  * can be customized as necessary based on the application's needs</li>
  * <li>Default per-instance behavior can be customized on implementations, typically via JavaBeans mutators.</li>
  * <li>Perhaps most important for Shiro end-users, tt can more easily be used as a source of cryptographic seed data,
- * and the data returned is already in a more {@link ByteSource ByteSource} format in case that data needs to be
- * {@link org.apache.shiro.util.ByteSource#toHex() hex} or
+ * and the data returned is already in a more convenient {@link ByteSource ByteSource} format in case that data needs
+ * to be {@link org.apache.shiro.util.ByteSource#toHex() hex} or
  * {@link org.apache.shiro.util.ByteSource#toBase64() base64}-encoded.</li>
  * </ul>
  * For example, consider the following example generating password salts for new user accounts:
- *
  * <pre>
  * RandomNumberGenerator saltGenerator = new {@link org.apache.shiro.crypto.SecureRandomNumberGenerator SecureRandomNumberGenerator}();
  * User user = new User();
@@ -53,15 +52,21 @@ public interface RandomNumberGenerator {
      * Generates a byte array of fixed length filled with random data, often useful for generating salts,
      * initialization vectors or other seed data.  The length is specified as a configuration
      * value on the underlying implementation.
+     * <p/>
+     * If you'd like per-invocation control the number of bytes generated, use the
+     * {@link #nextBytes(int) nextBytes(int)} method instead.
      *
      * @return a byte array of fixed length filled with random data.
+     * @see #nextBytes(int)
      */
     ByteSource nextBytes();
 
     /**
      * Generates a byte array of the specified length filled with random data.
+     *
      * @param numBytes the number of bytes to be populated with random data.
      * @return a byte array of the specified length filled with random data.
+     * @see #nextBytes()
      */
     ByteSource nextBytes(int numBytes);
 }
