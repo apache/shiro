95ab542d88973b80b7fe544586565bbd3992f6de
Les Hazlewood <lhazlewood@apache.org>
2011-06-26 23:26:16 +0000

    JavaDoc and license header updates.
    git-svn-id: https://svn.apache.org/repos/asf/shiro/trunk@1139969 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/shiro/mgt/SubjectFactory.java
+++ core/src/main/java/org/apache/shiro/mgt/SubjectFactory.java
@@ -24,7 +24,7 @@ import org.apache.shiro.subject.SubjectContext;
 import java.util.Map;
 
 /**
- * A {@code SubjectFactory} is responsible for returning {@link Subject Subject} instances as needed.
+ * A {@code SubjectFactory} is responsible for constructing {@link Subject Subject} instances as needed.
  *
  * @since 1.0
  */
@@ -33,12 +33,15 @@ public interface SubjectFactory {
     /**
      * Creates a new Subject instance reflecting the state of the specified contextual data.  The data would be
      * anything required to required to construct a {@code Subject} instance and its contents can vary based on
-     * environment.  Any data supported by Shiro core will be accessible by one of the accessor methods.  All other
-     * data is available as map {@link Map#get attribute}s.
+     * environment.
+     * <p/>
+     * Any data supported by Shiro core will be accessible by one of the {@code SubjectContext}'s {@code get*}
+     * or {@code resolve*} methods.  All other data is available as map {@link Map#get attribute}s.
      *
      * @param context the contextual data to be used by the implementation to construct an appropriate {@code Subject}
      *                instance.
      * @return a {@code Subject} instance created based on the specified context.
+     * @see SubjectContext
      */
     Subject createSubject(SubjectContext context);
 
