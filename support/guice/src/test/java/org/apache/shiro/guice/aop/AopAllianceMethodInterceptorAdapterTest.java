b8965b71d6135495dc9eb651d67fe52604540dd2
Kalle Korhonen <kaosko@apache.org>
2011-07-18 11:10:51 +0000

    SHIRO-23 - Integrating Jsecurity with Guice: - apply Jared Bunting's guice-final.patch with no other changes but added license info - rat run passes
    git-svn-id: https://svn.apache.org/repos/asf/shiro/trunk@1147810 13f79535-47bb-0310-9956-ffa450edef68

---
+++ support/guice/src/test/java/org/apache/shiro/guice/aop/AopAllianceMethodInterceptorAdapterTest.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.shiro.guice.aop;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.shiro.aop.MethodInterceptor;
+import org.easymock.IAnswer;
+import org.junit.Test;
+
+import static org.easymock.EasyMock.*;
+import static org.junit.Assert.assertSame;
+
+
+public class AopAllianceMethodInterceptorAdapterTest {
+    @Test
+    public void testInvoke() throws Throwable {
+        MethodInvocation allianceInvocation = createMock(MethodInvocation.class);
+        MethodInterceptor mockShiroInterceptor = createMock(MethodInterceptor.class);
+        expect(mockShiroInterceptor.invoke(anyObject(AopAllianceMethodInvocationAdapter.class))).andAnswer(new IAnswer<Object>() {
+            public Object answer() throws Throwable {
+                return getCurrentArguments()[0];
+            }
+        });
+        final Object expectedValue = new Object();
+        expect(allianceInvocation.proceed()).andReturn(expectedValue);
+
+        replay(mockShiroInterceptor, allianceInvocation);
+
+        AopAllianceMethodInterceptorAdapter underTest = new AopAllianceMethodInterceptorAdapter(mockShiroInterceptor);
+        Object invocation = underTest.invoke(allianceInvocation);
+        Object value = ((AopAllianceMethodInvocationAdapter) invocation).proceed();
+
+        assertSame("Adapter invocation returned a different value.", expectedValue, value);
+
+        verify(mockShiroInterceptor, allianceInvocation);
+    }
+}
