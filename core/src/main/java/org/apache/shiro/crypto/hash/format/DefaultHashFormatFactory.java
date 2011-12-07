5ad74735b4ecd61fdd44d1b77040b5e0e6721ec7
Les Hazlewood <lhazlewood@apache.org>
2011-12-07 20:38:53 +0000

    SHIRO-279: added tests to bring the org.apache.shiro.crypto.hash.format package to 100% class and line coverage.  Also added 'ProvidedHashFormat' enum to represent all out-of-the-box HashFormat implementations.
    git-svn-id: https://svn.apache.org/repos/asf/shiro/trunk@1211626 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/shiro/crypto/hash/format/DefaultHashFormatFactory.java
+++ core/src/main/java/org/apache/shiro/crypto/hash/format/DefaultHashFormatFactory.java
@@ -19,11 +19,8 @@
 package org.apache.shiro.crypto.hash.format;
 
 import org.apache.shiro.util.ClassUtils;
-import org.apache.shiro.util.CollectionUtils;
 import org.apache.shiro.util.StringUtils;
 import org.apache.shiro.util.UnknownClassException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -33,38 +30,83 @@ import java.util.Set;
 /**
  * This default {@code HashFormatFactory} implementation heuristically determines a {@code HashFormat} class to
  * instantiate based on the input argument and returns a new instance of the discovered class.  The heuristics are
- * detailed in the {@link #getInstance(String) getInstance} method documentation.j
+ * detailed in the {@link #getInstance(String) getInstance} method documentation.
  *
  * @since 1.2
  */
 public class DefaultHashFormatFactory implements HashFormatFactory {
 
-    private static final Logger log = LoggerFactory.getLogger(DefaultHashFormatFactory.class);
-
-    private static final String DEFAULT_HASH_FORMAT_PACKAGE_NAME = HashFormat.class.getPackage().getName();
-
     private Map<String, String> formatClassNames; //id - to - fully qualified class name
 
-    private Set<String> searchPackages;
+    private Set<String> searchPackages; //packages to search for HashFormat implementations
 
     public DefaultHashFormatFactory() {
         this.searchPackages = new HashSet<String>();
-        formatClassNames = new HashMap<String, String>();
-        formatClassNames.put(Shiro1CryptFormat.ID, Shiro1CryptFormat.class.getName());
+        this.formatClassNames = new HashMap<String, String>();
     }
 
+    /**
+     * Returns a {@code hashFormatAlias}-to-<code>fullyQualifiedHashFormatClassNameImplementation</code> map.
+     * <p/>
+     * This map will be used by the {@link #getInstance(String) getInstance} implementation:  that method's argument
+     * will be used as a lookup key to this map.  If the map returns a value, that value will be used to instantiate
+     * and return a new {@code HashFormat} instance.
+     * <h3>Defaults</h3>
+     * Shiro's default HashFormat implementations (as listed by the {@link ProvidedHashFormat} enum) will
+     * be searched automatically independently of this map.  You only need to populate this map with custom
+     * {@code HashFormat} implementations that are <em>not</em> already represented by a {@code ProvidedHashFormat}.
+     * <h3>Efficiency</h3>
+     * Populating this map will be more efficient than configuring {@link #getSearchPackages() searchPackages},
+     * but search packages may be more convenient depending on the number of {@code HashFormat} implementations that
+     * need to be supported by this factory.
+     *
+     * @return a {@code hashFormatAlias}-to-<code>fullyQualifiedHashFormatClassNameImplementation</code> map.
+     */
     public Map<String, String> getFormatClassNames() {
         return formatClassNames;
     }
 
+    /**
+     * Sets the {@code hash-format-alias}-to-{@code fullyQualifiedHashFormatClassNameImplementation} map to be used in
+     * the {@link #getInstance(String)} implementation.  See the {@link #getFormatClassNames()} JavaDoc for more
+     * information.
+     * <h3>Efficiency</h3>
+     * Populating this map will be more efficient than configuring {@link #getSearchPackages() searchPackages},
+     * but search packages may be more convenient depending on the number of {@code HashFormat} implementations that
+     * need to be supported by this factory.
+     *
+     * @param formatClassNames the {@code hash-format-alias}-to-{@code fullyQualifiedHashFormatClassNameImplementation}
+     *                         map to be used in the {@link #getInstance(String)} implementation.
+     */
     public void setFormatClassNames(Map<String, String> formatClassNames) {
         this.formatClassNames = formatClassNames;
     }
 
+    /**
+     * Returns a set of package names that can be searched for {@link HashFormat} implementations according to
+     * heuristics defined in the {@link #getHashFormatClass(String, String) getHashFormat(packageName, token)} JavaDoc.
+     * <h3>Efficiency</h3>
+     * Configuring this property is not as efficient as configuring a {@link #getFormatClassNames() formatClassNames}
+     * map, but it may be more convenient depending on the number of {@code HashFormat} implementations that
+     * need to be supported by this factory.
+     *
+     * @return a set of package names that can be searched for {@link HashFormat} implementations
+     * @see #getHashFormatClass(String, String)
+     */
     public Set<String> getSearchPackages() {
         return searchPackages;
     }
 
+    /**
+     * Sets a set of package names that can be searched for {@link HashFormat} implementations according to
+     * heuristics defined in the {@link #getHashFormatClass(String, String) getHashFormat(packageName, token)} JavaDoc.
+     * <h3>Efficiency</h3>
+     * Configuring this property is not as efficient as configuring a {@link #getFormatClassNames() formatClassNames}
+     * map, but it may be more convenient depending on the number of {@code HashFormat} implementations that
+     * need to be supported by this factory.
+     *
+     * @param searchPackages a set of package names that can be searched for {@link HashFormat} implementations
+     */
     public void setSearchPackages(Set<String> searchPackages) {
         this.searchPackages = searchPackages;
     }
@@ -75,13 +117,14 @@ public class DefaultHashFormatFactory implements HashFormatFactory {
         }
 
         HashFormat hashFormat = null;
+        Class clazz = null;
 
-        Class clazz = getHashFormatClass(in);
-
-        //The 'in' argument didn't result in a corresponding HashFormat class using our heuristics.
-        //As a fallback, check to see if the argument is an MCF-formatted string.  If it is, odds are very high
-        //that the MCF ID id is the lookup token we can use to find a corresponding HashFormat class:
-        if (clazz == null && in.startsWith(ModularCryptFormat.TOKEN_DELIMITER)) {
+        //NOTE: this code block occurs BEFORE calling getHashFormatClass(in) on purpose as a performance
+        //optimization.  If the input arg is an MCF-formatted string, there will be many unnecessary ClassLoader
+        //misses which can be slow.  By checking the MCF-formatted option, we can significantly improve performance
+        if (in.startsWith(ModularCryptFormat.TOKEN_DELIMITER)) {
+            //odds are high that the input argument is not a fully qualified class name or a format key (e.g. 'hex',
+            //base64' or 'shiro1').  Try to find the key and lookup via that:
             String test = in.substring(ModularCryptFormat.TOKEN_DELIMITER.length());
             String[] tokens = test.split("\\" + ModularCryptFormat.TOKEN_DELIMITER);
             //the MCF ID is always the first token in the delimited string:
@@ -92,28 +135,14 @@ public class DefaultHashFormatFactory implements HashFormatFactory {
             }
         }
 
+        if (clazz == null) {
+            //not an MCF-formatted string - use the unaltered input arg and go through our heuristics:
+            clazz = getHashFormatClass(in);
+        }
+
         if (clazz != null) {
             //we found a HashFormat class - instantiate it:
             hashFormat = newHashFormatInstance(clazz);
-
-            //do further compatibility testing if we can:
-            if (hashFormat instanceof ParsableHashFormat) {
-                //This is not really an efficient way to test for format compatibility, but
-                //there is no other way that guarantees compatibility that I can think of at the moment.
-                //perhaps an isCompatible method can be introduced?  The struggle I have with this is how do you
-                //determine compatibility without parsing it fully?  If not fully parsed, then it truly can't be
-                //guaranteed compatible - at which point, you might as well just parse the thing - L.H. 22 Nov 2011
-                try {
-                    ParsableHashFormat phf = (ParsableHashFormat)hashFormat;
-                    phf.parse(in);
-                    // no exception - must be a match:
-                    return phf;
-                } catch (RuntimeException re) {
-                    log.debug("Candidate format instance of type [{}] is unable to " +
-                            "parse formatted String [{}].  Ignoring.", clazz, in);
-                    log.trace("HashFormat parsing caused exception: ", re);
-                }
-            }
         }
 
         return hashFormat;
@@ -123,40 +152,63 @@ public class DefaultHashFormatFactory implements HashFormatFactory {
      * Heuristically determine the fully qualified HashFormat implementation class name based on the specified
      * token.
      * <p/>
-     * This implementation functions as follows:
+     * This implementation functions as follows (in order):
+     * <ol>
+     * <li>See if the argument can be used as a lookup key in the {@link #getFormatClassNames() formatClassNames}
+     * map.  If a value (a fully qualified class name {@link HashFormat HashFormat} implementation) is found,
+     * {@link ClassUtils#forName(String) lookup} the class and return it.</li>
+     * <li>
+     * Check to see if the token argument is a
+     * {@link ProvidedHashFormat} enum value.  If so, acquire the corresponding {@code HashFormat} class and
+     * return it.
+     * </li>
+     * <li>
+     * Check to see if the token argument is itself a fully qualified class name.  If so, try to load the class
+     * and return it.
+     * </li>
+     * <li>If the above options do not result in a discovered class, search all all configured
+     * {@link #getSearchPackages() searchPackages} using heuristics defined in the
+     * {@link #getHashFormatClass(String, String) getHashFormatClass(packageName, token)} method documentation
+     * (relaying the {@code token} argument to that method for each configured package).
+     * </li>
+     * </ol>
      * <p/>
-     * All configured {@link #getSearchPackages() searchPackages} will be searched using heuristics defined in the
-     * {@link #getHashFormatClass(String, String) getHashFormatClass(packageName, token)} method documentation (relaying
-     * the {@code token} argument to that method for each configured package).
-     * <p/>
-     * If the class was not found in any configured {@code searchPackages}, the default
-     * {@code org.apache.shiro.crypto.hash.format} package will be attempted as a final fallback.
-     * </p>
-     * If the class was not discovered in any of the {@code searchPackages} or in Shiro's default fallback package,
-     * {@code null} is returned to indicate the class could not be found.
+     * If a class is not discovered via any of the above means, {@code null} is returned to indicate the class
+     * could not be found.
      *
      * @param token the string token from which a class name will be heuristically determined.
      * @return the discovered HashFormat class implementation or {@code null} if no class could be heuristically determined.
      */
     protected Class getHashFormatClass(String token) {
 
-        //check to see if the token is a fully qualified class name:
-        Class clazz = lookupHashFormatClass(token);
+        Class clazz = null;
 
+        //check to see if the token is a configured FQCN alias.  This is faster than searching packages,
+        //so we try this first:
+        if (this.formatClassNames != null) {
+            String value = this.formatClassNames.get(token);
+            if (value != null) {
+                //found an alias - see if the value is a class:
+                clazz = lookupHashFormatClass(value);
+            }
+        }
+
+        //check to see if the token is one of Shiro's provided FQCN aliases (again, faster than searching):
         if (clazz == null) {
-            //check to see if the token is a FQCN alias:
-            if (!CollectionUtils.isEmpty(this.formatClassNames)) {
-                String value = this.formatClassNames.get(token);
-                if (value != null) {
-                    //found an alias - see if the value is a class:
-                    clazz = lookupHashFormatClass(token);
-                }
+            ProvidedHashFormat provided = ProvidedHashFormat.byId(token);
+            if (provided != null) {
+                clazz = provided.getHashFormatClass();
             }
         }
 
         if (clazz == null) {
+            //check to see if 'token' was a FQCN itself:
+            clazz = lookupHashFormatClass(token);
+        }
+
+        if (clazz == null) {
             //token wasn't a FQCN or a FQCN alias - try searching in configured packages:
-            if (!CollectionUtils.isEmpty(this.searchPackages)) {
+            if (this.searchPackages != null) {
                 for (String packageName : this.searchPackages) {
                     clazz = getHashFormatClass(packageName, token);
                     if (clazz != null) {
@@ -165,11 +217,6 @@ public class DefaultHashFormatFactory implements HashFormatFactory {
                     }
                 }
             }
-        }
-
-        if (clazz == null) {
-            //couldn't find it in any configured search packages.  Try Shiro's default search package:
-            clazz = getHashFormatClass(DEFAULT_HASH_FORMAT_PACKAGE_NAME, token);
         }
 
         if (clazz != null) {
@@ -302,6 +349,6 @@ public class DefaultHashFormatFactory implements HashFormatFactory {
 
     protected final HashFormat newHashFormatInstance(Class clazz) {
         assertHashFormatImpl(clazz);
-        return (HashFormat)ClassUtils.newInstance(clazz);
+        return (HashFormat) ClassUtils.newInstance(clazz);
     }
 }
