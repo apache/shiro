9a16dfd25a3305eb7e6db378dd76c004058c2784
Les Hazlewood <lhazlewood@apache.org>
2012-01-18 23:15:02 +0000

    SHIRO-339: preparing for 1.2 release.  Added license headers per RAT plugin.
    git-svn-id: https://svn.apache.org/repos/asf/shiro/trunk@1233109 13f79535-47bb-0310-9956-ffa450edef68

--- web/src/test/java/org/apache/shiro/web/filter/PathMatchingFilterTest.java
+++ web/src/test/java/org/apache/shiro/web/filter/PathMatchingFilterTest.java
@@ -1,16 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.shiro.web.filter;
 
-import org.apache.shiro.web.servlet.OncePerRequestFilter;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.servlet.FilterChain;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
 import static org.easymock.EasyMock.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit tests for the {@link PathMatchingFilter} implementation.
