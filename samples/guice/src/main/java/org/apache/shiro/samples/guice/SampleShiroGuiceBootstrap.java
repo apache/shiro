a67dea3b3a225e5fd84585b0810ffed66750eb5d
Jared Bunting <jbunting@apache.org>
2012-06-04 13:26:34 +0000

    SHIRO-320: creating samples-guice as a port of samples-web, modified for guice integtaion
    git-svn-id: https://svn.apache.org/repos/asf/shiro/trunk@1345958 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/guice/src/main/java/org/apache/shiro/samples/guice/SampleShiroGuiceBootstrap.java
@@ -0,0 +1,25 @@
+package org.apache.shiro.samples.guice;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+import org.apache.shiro.guice.web.ShiroWebModule;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+
+public class SampleShiroGuiceBootstrap extends GuiceServletContextListener {
+
+    private ServletContext servletContext;
+
+    @Override
+    public void contextInitialized(final ServletContextEvent servletContextEvent) {
+        this.servletContext = servletContextEvent.getServletContext();
+        super.contextInitialized(servletContextEvent);
+    }
+
+    @Override
+    protected Injector getInjector() {
+        return Guice.createInjector(new SampleShiroServletModule(servletContext), ShiroWebModule.guiceFilterModule());
+    }
+}
