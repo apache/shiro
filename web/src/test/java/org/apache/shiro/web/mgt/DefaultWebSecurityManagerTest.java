2714fcdf82c23990c8426e091ab71a9235fc969d
Les Hazlewood <lhazlewood@apache.org>
2012-07-24 17:30:21 +0000

    Merged from 1.2.x SHIRO-350: Prevented session storage when the subject is a non-web subject and the session manager is a web-only session manager. [from revision 1365167]
    git-svn-id: https://svn.apache.org/repos/asf/shiro/trunk@1365184 13f79535-47bb-0310-9956-ffa450edef68

--- web/src/test/java/org/apache/shiro/web/mgt/DefaultWebSecurityManagerTest.java
+++ web/src/test/java/org/apache/shiro/web/mgt/DefaultWebSecurityManagerTest.java
@@ -24,7 +24,10 @@ import org.apache.shiro.realm.text.IniRealm;
 import org.apache.shiro.session.ExpiredSessionException;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.session.mgt.AbstractSessionManager;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.SimplePrincipalCollection;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.config.WebIniSecurityManagerFactory;
 import org.apache.shiro.web.servlet.ShiroHttpSession;
 import org.apache.shiro.web.session.mgt.WebSessionManager;
 import org.apache.shiro.web.subject.WebSubject;
@@ -209,6 +212,27 @@ public class DefaultWebSecurityManagerTest extends AbstractWebSecurityManagerTes
 
         verify(mockRequest);
         verify(mockResponse);
+    }
+
+    /**
+     * Asserts fix for <a href="https://issues.apache.org/jira/browse/SHIRO-350">SHIRO-350</a>.
+     */
+    @Test
+    public void testBuildNonWebSubjectWithDefaultServletContainerSessionManager() {
+
+        Ini ini = new Ini();
+        Ini.Section section = ini.addSection(IniRealm.USERS_SECTION_NAME);
+        section.put("user1", "user1");
+
+        WebIniSecurityManagerFactory factory = new WebIniSecurityManagerFactory(ini);
+
+        WebSecurityManager securityManager = (WebSecurityManager)factory.getInstance();
+
+        PrincipalCollection principals = new SimplePrincipalCollection("user1", "iniRealm");
+        Subject subject = new Subject.Builder(securityManager).principals(principals).buildSubject();
+
+        assertNotNull(subject);
+        assertEquals("user1", subject.getPrincipal());
     }
 
 }
