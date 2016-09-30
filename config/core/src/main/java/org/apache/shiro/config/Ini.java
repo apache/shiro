6e9a20abc65acf9db25c2667d114230d74513416
Brian Demers <bdemers@apache.org>
2016-09-30 16:40:56 -0400

    SHIRO-593 - Added getFrameworkIni() method to IniWebEnvironment
    Which will allow downstream integrations to provide default configuration that will be
    merged with the existing configuration.  Default functionality is unchanged.

--- config/core/src/main/java/org/apache/shiro/config/Ini.java
+++ config/core/src/main/java/org/apache/shiro/config/Ini.java
@@ -303,6 +303,57 @@ public class Ini implements Map<String, Ini.Section> {
         }
     }
 
+    /**
+     * Merges the contents of <code>m</code>'s {@link Section} objects into self.
+     * This differs from {@link Ini#putAll(Map)}, in that each section is merged with the existing one.
+     * For example the following two ini blocks are merged and the result is the third<BR/>
+     * <p>
+     * Initial:
+     * <pre>
+     * <code>[section1]
+     * key1 = value1
+     *
+     * [section2]
+     * key2 = value2
+     * </code> </pre>
+     *
+     * To be merged:
+     * <pre>
+     * <code>[section1]
+     * foo = bar
+     *
+     * [section2]
+     * key2 = new value
+     * </code> </pre>
+     *
+     * Result:
+     * <pre>
+     * <code>[section1]
+     * key1 = value1
+     * foo = bar
+     *
+     * [section2]
+     * key2 = new value
+     * </code> </pre>
+     *
+     * </p>
+     *
+     * @param m map to be merged
+     * @since 1.4
+     */
+    public void merge(Map<String, Section> m) {
+
+        if (m != null) {
+            for (Entry<String, Section> entry : m.entrySet()) {
+                Section section = this.getSection(entry.getKey());
+                if (section == null) {
+                    section = addSection(entry.getKey());
+                }
+                section.putAll(entry.getValue());
+            }
+        }
+    }
+
     private void addSection(String name, StringBuilder content) {
         if (content.length() > 0) {
             String contentString = content.toString();
