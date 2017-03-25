3ebfd9595e1bc03a320e49b8a8d2c6941acaf6cf
marcgiffing <marcgiffing@web.de>
2017-03-25 16:14:40 +0100

    SHIRO-618 - Autoconfiguration for Realm and ShiroFilterChainDefinition

---
+++ support/spring-boot/spring-boot-web-starter/src/main/java/org/apache/shiro/spring/config/web/autoconfigure/exception/NoRealmBeanConfiguredException.java
@@ -0,0 +1,9 @@
+package org.apache.shiro.spring.config.web.autoconfigure.exception;
+
+/**
+ * This exception should be thrown if not bean of type {@link Realm} found.
+ *
+ */
+public class NoRealmBeanConfiguredException extends RuntimeException {
+
+}
