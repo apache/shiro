15cfa05729be72a696648be7e35939dbf92c449e
Martin Nybo Nielsen <mnybon@gmail.com>
2018-04-09 18:16:32 +0200

    Removed nits: Whitespace on empty line and auto-generated author tag

--- web/src/test/java/org/apache/shiro/web/mgt/NonIniWebSecurityManagerTest.java
+++ web/src/test/java/org/apache/shiro/web/mgt/NonIniWebSecurityManagerTest.java
@@ -13,10 +13,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- *
- * @author mnn
- */
 public class NonIniWebSecurityManagerTest extends AbstractWebSecurityManagerTest {
     
     private DefaultWebSecurityManager sm;
