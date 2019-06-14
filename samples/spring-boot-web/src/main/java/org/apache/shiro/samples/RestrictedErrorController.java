8767b7dcda191dfb9e3a3a016839ee2a960026fb
Colm O hEigeartaigh <coheigea@apache.org>
2019-06-14 11:43:28 +0100

    SHIRO-704 - Update Spring, Spring Boot + Hibernate

--- samples/spring-boot-web/src/main/java/org/apache/shiro/samples/RestrictedErrorController.java
+++ samples/spring-boot-web/src/main/java/org/apache/shiro/samples/RestrictedErrorController.java
@@ -19,12 +19,12 @@
 package org.apache.shiro.samples;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.ErrorAttributes;
-import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.request.ServletWebRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
@@ -45,7 +45,7 @@ public class RestrictedErrorController implements ErrorController {
 
     @RequestMapping(ERROR_PATH)
     String error(HttpServletRequest request, Model model) {
-        Map<String, Object> errorMap = errorAttributes.getErrorAttributes(new ServletRequestAttributes(request), false);
+        Map<String, Object> errorMap = errorAttributes.getErrorAttributes(new ServletWebRequest(request), false);
         model.addAttribute("errors", errorMap);
         return "error";
     }
