c539537ab90f56fdb38066e7177184b189106015
Brian Demers <bdemers@apache.org>
2020-11-09 14:18:04 -0500

    Add basic tests to assert no whitespace in spring.factories fiels

---
+++ support/spring-boot/spring-boot-web-starter/src/test/groovy/org/apache/shiro/spring/boot/autoconfigure/web/WebSpringFactoriesTest.groovy
@@ -0,0 +1,24 @@
+package org.apache.shiro.spring.boot.autoconfigure.web
+
+import org.junit.Test
+
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.Matchers.matchesPattern
+import static org.hamcrest.Matchers.not
+
+class WebSpringFactoriesTest {
+
+    @Test
+    void springFactoriesConfigContainsNoWhitespace() {
+        Properties props = new Properties()
+        props.load(new FileReader("src/main/resources/META-INF/spring.factories"))
+        assertNoWhitespaceInEntries(props)
+    }
+
+    static private assertNoWhitespaceInEntries(Properties props) {
+        props.each{ key, val ->
+            assertThat "Property [${key}] contains whitespace",
+            props.get("org.springframework.boot.autoconfigure.EnableAutoConfiguration"), not(matchesPattern(".*\\s.*"))
+        }
+    }
+}
