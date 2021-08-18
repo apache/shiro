e1b7802ecc0f0c34d9ee766a19705926a8264e23
Brian Demers <bdemers@apache.org>
2021-08-18 17:58:20 -0400

    Add @Documented to Shiro annotations
    Shiro annotations will now be included in generated Javadoc
    
    Fixes: SHIRO-216

--- core/src/main/java/org/apache/shiro/authz/annotation/RequiresRoles.java
+++ core/src/main/java/org/apache/shiro/authz/annotation/RequiresRoles.java
@@ -18,6 +18,7 @@
  */
 package org.apache.shiro.authz.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -52,6 +53,7 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface RequiresRoles {
 
     /**
