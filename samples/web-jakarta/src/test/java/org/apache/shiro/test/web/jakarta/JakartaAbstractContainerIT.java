287ede85293af3578aaf4395c7cea1a7d1b89bac
Rui Ventura <rui.ventura@causeway.com>
2025-03-03 23:46:44 +0000

    Properly disable meecrowave-based IT

--- samples/web-jakarta/src/test/java/org/apache/shiro/test/web/jakarta/JakartaAbstractContainerIT.java
+++ samples/web-jakarta/src/test/java/org/apache/shiro/test/web/jakarta/JakartaAbstractContainerIT.java
@@ -24,11 +24,9 @@ import org.junit.jupiter.api.BeforeAll;
 
 import java.io.File;
 import java.io.FilenameFilter;
-import org.junit.jupiter.api.Disabled;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Disabled("Needs meecrowave 2.0.0")
 public abstract class JakartaAbstractContainerIT {
 
     protected static Meecrowave meecrowave;
